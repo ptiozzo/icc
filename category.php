d48af9500671e0c69cb28cbca4a6b0bc0396567f
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-08-19 22:55:19 +0200

    Aggiunta paginazione per le categorie

--- category.php
+++ category.php
@@ -8,8 +8,24 @@
 
   <a href="<?php echo the_permalink();?>"><?php echo get_the_title(); ?></a><br />
 
+
   <?php
+
       endwhile;
+ ?>
+      <div class="pagination">
+      <br />
+        <?php
+          $big = 999999999; // need an unlikely integer
+          echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages
+          ) );
+        ?>
+      </div><!-- .pagination -->
+<?php
   else:
   ?>
 
