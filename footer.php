05f61f1e632aa3559fd5fb7a8d10751b801172d3
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-08-21 23:26:01 +0200

    Aggiunto DIV per impaginazione

--- footer.php
+++ footer.php
@@ -1,12 +1,14 @@
 <div class="clearfix"></div>
 
 	<footer class="footer">
-		<div class="img-footer">Immagine footer</div>
-		<div class="rag-soc-footer">Ragione sociale footer</div>
-		<?php
-			wp_nav_menu( array('theme_location' => 'menu-social'));
-			wp_nav_menu( array('theme_location' => 'menu-footer'));
-		?>
+		<div class="footer-content">
+			<div class="img-footer">Immagine footer</div>
+			<div class="rag-soc-footer">Ragione sociale footer</div>
+			<?php
+				wp_nav_menu( array('theme_location' => 'menu-social'));
+				wp_nav_menu( array('theme_location' => 'menu-footer'));
+			?>
+		</div> <!-- .footer-content -->	
 	</footer>
 	<?php wp_footer();?>
 </div> <!-- container -->
