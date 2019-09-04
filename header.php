cbda18c8d16476f66536deb481a5719de998aa2a
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-09-04 19:57:00 +0200

    Modificato menu scomparsa

--- header.php
+++ header.php
@@ -22,6 +22,11 @@
             <?php
             wp_nav_menu( array('theme_location' => 'menu-i-nostri-contenuti'));
             wp_nav_menu( array('theme_location' => 'menu-regioni'));?>
+            <div class="header__menu__scomparsa__ragionesociale">
+              <strong>CF:</strong> 97761390588</br>
+              <strong>P.IVA:</strong> 12511651007</br>
+            </div>
+            <?php wp_nav_menu( array('theme_location' => 'menu-social'));?>
           </div> <!-- header__menu__scomparsa__menunascosto -->
             <div class="header__menu__scomparsa__descrizione">
               informarsi<br />
