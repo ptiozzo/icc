54ba5b549a7686484994b53eadf9a34997b45f63
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-09-04 20:00:56 +0200

    Add page content

--- page.php
+++ page.php
@@ -1,4 +1,24 @@
 <?php get_header(); ?>
-page
+<div class="content">
+  <?php if( have_posts() ) : ?>
+    <?php
+      while(have_posts() ) : the_post();
+  ?>
+
+  <h1><?php echo get_the_title(); ?></h1>
+
+  <?php echo the_content(); ?>
+
+  <?php
+      endwhile;
+  else:
+  ?>
+
+  <p>Non ho trovato nulla</p>
+
+  <?php
+  endif;
+  ?>
+</div> <!-- .content 	-->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
