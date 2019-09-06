afc50ee962297c1689b0d3dcbdf67824c0257b3c
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-09-06 22:24:54 +0200

    Spostato attivati e custom post in file differenti

--- functions.php
+++ functions.php
@@ -69,90 +69,9 @@ if ( ! function_exists( 'icc_sidebars' ) ) {
 }
 add_action( 'widgets_init', 'icc_sidebars' );
 
-/*  Custom post type Campagne tematiche
+/*  Custom post type
 /* ------------------------------------ */
-function icc_campagne_tematiche_custom_post() {
-    // creo e registro il custom post type
-    register_post_type('campagne-tematiche', /* nome del custom post type */
-        // definisco le varie etichette da mostrare nei menù
-        array('labels' => array(
-            'name' => 'Campagne Tematiche', /* nome, al plurale, dell'etichetta del post type. */
-            'singular_name' => 'Campagna Tematica', /* nome, al singolare, dell'etichetta del post type. */
-            'all_items' => 'Tutte le Campagne tematiche', /* testo nei menu che indica tutti i contenuti del post type */
-            'add_new' => 'Aggiungi nuova campagna tematica', /*testo del pulsante Aggiungi. */
-            'add_new_item' => 'Aggiungi nuova campagna', /* testo per il pulsante Aggiungi nuovo post type */
-            'edit_item' => 'Modifica', /*  testo modifica */
-            'new_item' => 'Nuovo', /* testo nuovo oggetto */
-            'view_item' => 'Visualizza', /* testo per visualizzare */
-            'search_items' => 'Cerca', /* testo per la ricerca*/
-            'not_found' =>  'Nessuna campagna tematica trovata', /* testo se non trova nulla */
-            'not_found_in_trash' => 'Nessuna campagna tematica trovata nel cestino', /* testo se non trova nulla nel cestino */
-            'parent_item_colon' => ''
-            ), /* fine dell'array delle etichette del menu */
-            'description' => 'Campagne Tematiche', /* descrizione del post type */
-            'public' => true, /* definisce se il post type sia visibile sia da front-end che da back-end */
-            'publicly_queryable' => true, /* definisce se possono essere fatte query da front-end */
-            'exclude_from_search' => false, /* esclude (false) il post type dai risultati di ricerca */
-            'show_ui' => true, /* definisce se deve essere visualizzata l'interfaccia di default nel pannello di amministrazione */
-            'query_var' => true,
-            'menu_position' => 8, /* definisce l'ordine in cui comparire nel menù di amministrazione a sinistra */
-            'menu_icon' => 'dashicons-layout', /* imposta l'icona da usare nel menù per il posty type */
-            'rewrite'   => array( 'slug' => 'campagne-tematiche', 'with_front' => false ), /* specificare uno slug per leURL */
-						'show_in_rest' => 'true', /* abilita gutemberg come editor */
-            'has_archive' => true, /* definisci se abilitare la generazione di un archivio (tipo archive-cd.php) */
-            'capability_type' => 'post', /* definisci se si comporterà come un post o come una pagina */
-            'hierarchical' => false, /* definisci se potranno essere definiti elementi padri di altri */
-            /* la riga successiva definisce quali elementi verranno visualizzati nella schermata di creazione del post */
-            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
-        ) /* fine delle opzioni */
-    ); /* fine della registrazione */
-
-}
-// Inizializzo la funzione
-add_action( 'init', 'icc_campagne_tematiche_custom_post');
-
-
-/*  Custom post type I nostri libri
-/* ------------------------------------ */
-function icc_nostri_libri_custom_post() {
-    // creo e registro il custom post type
-    register_post_type('nostri-libri', /* nome del custom post type */
-        // definisco le varie etichette da mostrare nei menù
-        array('labels' => array(
-            'name' => 'I nostri libri', /* nome, al plurale, dell'etichetta del post type. */
-            'singular_name' => 'Nostro libro', /* nome, al singolare, dell'etichetta del post type. */
-            'all_items' => 'Tutti i libri', /* testo nei menu che indica tutti i contenuti del post type */
-            'add_new' => 'Aggiungi nuovo libro', /*testo del pulsante Aggiungi. */
-            'add_new_item' => 'Aggiungi libro', /* testo per il pulsante Aggiungi nuovo post type */
-            'edit_item' => 'Modifica', /*  testo modifica */
-            'new_item' => 'Nuovo', /* testo nuovo oggetto */
-            'view_item' => 'Visualizza', /* testo per visualizzare */
-            'search_items' => 'Cerca', /* testo per la ricerca*/
-            'not_found' =>  'Nessuna libro trovato trovata', /* testo se non trova nulla */
-            'not_found_in_trash' => 'Nessun libro trovato nel cestino', /* testo se non trova nulla nel cestino */
-            'parent_item_colon' => ''
-            ), /* fine dell'array delle etichette del menu */
-            'description' => 'I nostri libri', /* descrizione del post type */
-            'public' => true, /* definisce se il post type sia visibile sia da front-end che da back-end */
-            'publicly_queryable' => true, /* definisce se possono essere fatte query da front-end */
-            'exclude_from_search' => false, /* esclude (false) il post type dai risultati di ricerca */
-            'show_ui' => true, /* definisce se deve essere visualizzata l'interfaccia di default nel pannello di amministrazione */
-            'query_var' => true,
-            'menu_position' => 8, /* definisce l'ordine in cui comparire nel menù di amministrazione a sinistra */
-            'menu_icon' => 'dashicons-layout', /* imposta l'icona da usare nel menù per il posty type */
-            'rewrite'   => array( 'slug' => 'nostri-libri', 'with_front' => false ), /* specificare uno slug per leURL */
-						'show_in_rest' => 'true', /* abilita gutemberg come editor */
-            'has_archive' => true, /* definisci se abilitare la generazione di un archivio (tipo archive-cd.php) */
-            'capability_type' => 'post', /* definisci se si comporterà come un post o come una pagina */
-            'hierarchical' => false, /* definisci se potranno essere definiti elementi padri di altri */
-            /* la riga successiva definisce quali elementi verranno visualizzati nella schermata di creazione del post */
-            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
-        ) /* fine delle opzioni */
-    ); /* fine della registrazione */
-
-}
-// Inizializzo la funzione
-add_action( 'init', 'icc_nostri_libri_custom_post');
+require 'inc/custom-post.php';
 
 /*  Pagina istruzioni su admin
 /* ------------------------------------ */
@@ -197,36 +116,10 @@ add_action('init', 'start_session', 1);
 
 /* Aggiunta banner ATTIVATI dopo secondo paragrafo.
 /* ------------------------------------ */
-add_filter( 'the_content', 'prefix_insert_post_ads' );
-
-function prefix_insert_post_ads( $content ) {
-    $ad_code = '<div class="single__attivati">ATTIVATI</div>';
-    if ( is_single() && ! is_admin() ) {
-        return prefix_insert_after_paragraph( $ad_code, 2, $content );
-    }
-    return $content;
-}
-
-// Parent Function that makes the magic happen
-function prefix_insert_after_paragraph( $insertion, $paragraph_id, $content ) {
-    $closing_p = '</p>';
-    $paragraphs = explode( $closing_p, $content );
-    foreach ($paragraphs as $index => $paragraph) {
-
-        if ( trim( $paragraph ) ) {
-            $paragraphs[$index] .= $closing_p;
-        }
-
-        if ( $paragraph_id == $index + 1 ) {
-            $paragraphs[$index] .= $insertion;
-        }
-    }
-
-    return implode( '', $paragraphs );
-}
+require 'inc/attivati.php';
 
 
-/* Widget per Campagne Tematiche.
+/* Aggiunta dei Widget.
 /* ------------------------------------ */
 require 'inc/widget.php';
 
---
+++ inc/custom-post.php
@@ -0,0 +1,89 @@
+<?php
+
+
+/*  Custom post type Campagne tematiche
+/* ------------------------------------ */
+function icc_campagne_tematiche_custom_post() {
+    // creo e registro il custom post type
+    register_post_type('campagne-tematiche', /* nome del custom post type */
+        // definisco le varie etichette da mostrare nei menù
+        array('labels' => array(
+            'name' => 'Campagne Tematiche', /* nome, al plurale, dell'etichetta del post type. */
+            'singular_name' => 'Campagna Tematica', /* nome, al singolare, dell'etichetta del post type. */
+            'all_items' => 'Tutte le Campagne tematiche', /* testo nei menu che indica tutti i contenuti del post type */
+            'add_new' => 'Aggiungi nuova campagna tematica', /*testo del pulsante Aggiungi. */
+            'add_new_item' => 'Aggiungi nuova campagna', /* testo per il pulsante Aggiungi nuovo post type */
+            'edit_item' => 'Modifica', /*  testo modifica */
+            'new_item' => 'Nuovo', /* testo nuovo oggetto */
+            'view_item' => 'Visualizza', /* testo per visualizzare */
+            'search_items' => 'Cerca', /* testo per la ricerca*/
+            'not_found' =>  'Nessuna campagna tematica trovata', /* testo se non trova nulla */
+            'not_found_in_trash' => 'Nessuna campagna tematica trovata nel cestino', /* testo se non trova nulla nel cestino */
+            'parent_item_colon' => ''
+            ), /* fine dell'array delle etichette del menu */
+            'description' => 'Campagne Tematiche', /* descrizione del post type */
+            'public' => true, /* definisce se il post type sia visibile sia da front-end che da back-end */
+            'publicly_queryable' => true, /* definisce se possono essere fatte query da front-end */
+            'exclude_from_search' => false, /* esclude (false) il post type dai risultati di ricerca */
+            'show_ui' => true, /* definisce se deve essere visualizzata l'interfaccia di default nel pannello di amministrazione */
+            'query_var' => true,
+            'menu_position' => 8, /* definisce l'ordine in cui comparire nel menù di amministrazione a sinistra */
+            'menu_icon' => 'dashicons-layout', /* imposta l'icona da usare nel menù per il posty type */
+            'rewrite'   => array( 'slug' => 'campagne-tematiche', 'with_front' => false ), /* specificare uno slug per leURL */
+						'show_in_rest' => 'true', /* abilita gutemberg come editor */
+            'has_archive' => true, /* definisci se abilitare la generazione di un archivio (tipo archive-cd.php) */
+            'capability_type' => 'post', /* definisci se si comporterà come un post o come una pagina */
+            'hierarchical' => false, /* definisci se potranno essere definiti elementi padri di altri */
+            /* la riga successiva definisce quali elementi verranno visualizzati nella schermata di creazione del post */
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+        ) /* fine delle opzioni */
+    ); /* fine della registrazione */
+
+}
+// Inizializzo la funzione
+add_action( 'init', 'icc_campagne_tematiche_custom_post');
+
+
+/*  Custom post type I nostri libri
+/* ------------------------------------ */
+function icc_nostri_libri_custom_post() {
+    // creo e registro il custom post type
+    register_post_type('nostri-libri', /* nome del custom post type */
+        // definisco le varie etichette da mostrare nei menù
+        array('labels' => array(
+            'name' => 'I nostri libri', /* nome, al plurale, dell'etichetta del post type. */
+            'singular_name' => 'Nostro libro', /* nome, al singolare, dell'etichetta del post type. */
+            'all_items' => 'Tutti i libri', /* testo nei menu che indica tutti i contenuti del post type */
+            'add_new' => 'Aggiungi nuovo libro', /*testo del pulsante Aggiungi. */
+            'add_new_item' => 'Aggiungi libro', /* testo per il pulsante Aggiungi nuovo post type */
+            'edit_item' => 'Modifica', /*  testo modifica */
+            'new_item' => 'Nuovo', /* testo nuovo oggetto */
+            'view_item' => 'Visualizza', /* testo per visualizzare */
+            'search_items' => 'Cerca', /* testo per la ricerca*/
+            'not_found' =>  'Nessuna libro trovato trovata', /* testo se non trova nulla */
+            'not_found_in_trash' => 'Nessun libro trovato nel cestino', /* testo se non trova nulla nel cestino */
+            'parent_item_colon' => ''
+            ), /* fine dell'array delle etichette del menu */
+            'description' => 'I nostri libri', /* descrizione del post type */
+            'public' => true, /* definisce se il post type sia visibile sia da front-end che da back-end */
+            'publicly_queryable' => true, /* definisce se possono essere fatte query da front-end */
+            'exclude_from_search' => false, /* esclude (false) il post type dai risultati di ricerca */
+            'show_ui' => true, /* definisce se deve essere visualizzata l'interfaccia di default nel pannello di amministrazione */
+            'query_var' => true,
+            'menu_position' => 8, /* definisce l'ordine in cui comparire nel menù di amministrazione a sinistra */
+            'menu_icon' => 'dashicons-layout', /* imposta l'icona da usare nel menù per il posty type */
+            'rewrite'   => array( 'slug' => 'nostri-libri', 'with_front' => false ), /* specificare uno slug per leURL */
+						'show_in_rest' => 'true', /* abilita gutemberg come editor */
+            'has_archive' => true, /* definisci se abilitare la generazione di un archivio (tipo archive-cd.php) */
+            'capability_type' => 'post', /* definisci se si comporterà come un post o come una pagina */
+            'hierarchical' => false, /* definisci se potranno essere definiti elementi padri di altri */
+            /* la riga successiva definisce quali elementi verranno visualizzati nella schermata di creazione del post */
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+        ) /* fine delle opzioni */
+    ); /* fine della registrazione */
+
+}
+// Inizializzo la funzione
+add_action( 'init', 'icc_nostri_libri_custom_post');
+
+?>
