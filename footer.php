54fd60a57792251d4019dd9de60d035d72d37797
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-09-10 20:37:11 +0200

    rimozione vecchi file

--- footer.php
+++ footer.php
@@ -1,21 +1,73 @@
-<div class="clearfix"></div>
+</div>
 
-	<footer class="footer">
-		<div class="footer__content">
-			<div class="footer__logo">Immagine footer</div>
-			<div class="footer__ragionesociale">
-				ITALIA CHE CAMBIA</br>
-				Associazione di promozione sociale</br>
-				Via Aldo Banzi, 88 - 00128 Roma (RM)</br>
-				CF: 97761390588 P.IVA: 12511651007
-			</div>
-			<?php
-				wp_nav_menu( array('theme_location' => 'menu-social'));
-				wp_nav_menu( array('theme_location' => 'menu-footer'));
-			?>
-		</div> <!-- .footer__content -->
-	</footer>
-	<?php wp_footer();?>
-</div> <!-- container -->
+<footer>
+<section class='left-col'>
+<figure>
+  <img src='<?php echo get_template_directory_uri();?>/assets/img/logo/italia-che-cambia-footer.svg' title='' alt=''>
+</figure>
+<article>
+  <b>ITALIA CHE CAMBIA</b> <br>
+  <b>Associazione di promozione sociale</b> <br>
+  Via Aldo Banzi, 88 - 00128, Roma (RM) <br>
+  CF: 97761390588 | P. IVA: 12511651007 <br>
+</article>
+</section>
+<section class='right-col'>
+<ul class='socials'>
+  <li>
+    <a href=''>
+      <figure>
+        <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/twitter-grey.svg' title='' alt=''>
+      </figure>
+    </a>
+  </li>
+  <li>
+    <a href=''>
+      <figure>
+        <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/facebook-grey.svg' title='' alt=''>
+      </figure>
+    </a>
+  </li>
+  <li>
+    <a href=''>
+      <figure>
+        <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/youtube-grey.svg' title='' alt=''>
+      </figure>
+    </a>
+  </li>
+</ul>
+
+<ul class='links'>
+  <li>
+    <a href=''>
+      CREDITS
+    </a>
+  </li>
+  <li>
+    <a href=''>
+      COOKIES
+    </a>
+  </li>
+  <li>
+    <a href=''>
+      PRIVACY
+    </a>
+  </li>
+</ul>
+
+</section>
+</footer>
+
+
+<!-- Scripts
+<script src="/assets/js/plugins/jquery.min.js"></script>
+<script src="/assets/js/plugins/jquery.easing.min.js"></script>
+<script src="/assets/js/plugins/swiper.min.js"></script>
+<script src="/assets/js/app.js"></script>
+<script src="/assets/js/modules/home/index.js"></script>
+<script src="/assets/js/modules/salute-che-cambia/index.js"></script>
+<script src="/assets/js/modules/piemonte-che-cambia/index.js"></script>
+<script src="/assets/js/modules/contenuti/index.js"></script>
+-->
 </body>
 </html>
--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-
-	$(".header__menu__scomparsa-bar").click(function(e){
-		 $(".header__menu__scomparsa").toggleClass('show-menu');
-		 e.preventDefault();
-	});
-
-})( jQuery );
