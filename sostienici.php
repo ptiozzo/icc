9b53e1dbc194a0f8fb62417b5795aca89a8137aa
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-09-18 21:48:53 +0200

    Aggiunta pagina per sostienici

---
+++ sostienici.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Template Name: Sostienici
+*/
+?>
+
+<?php get_header(); ?>
+
+<main class="sostienici">
+	<section class="head">
+		<h1>Sostienici</h1>
+	</section>
+
+	<section class="content">
+		<div class="list">
+			<ul>
+				<li>
+					<article>
+						<h4>
+							DIVENTA UN AGENTE
+							DEL CAMBIAMENTO!
+							A ITALIA CHE CAMBIA
+						</h4>
+
+						<div class='description'>
+							<p>
+								Partecipando con l’1% del tuo stipendio, qualunque esso sia, contribuirai a sostenere le attività di Italia che Cambia orientate alla costruzione di un nuovo immaginario del nostro Paese, al racconto, mappatura e messa in rete dei processi in atto e realtà concrete del cambiamento italiano.
+								Grazie al tuo contributo, continueremo a produrre contenuti originali per ispirare attraverso le storie del cambiamento, la mappatura delle realtà significative, per vedere tutto quel che già c’è in Italia e a organizzare incontri sul territorio per creare nuove relazioni.
+							</p>
+							<p>
+								Collaborando possiamo davvero cambiare le cose!
+							</p>
+						</div>
+
+						<!--
+						<div class="cta">
+							<a href='' target='_blank'>ISCRIVITI ADESSO</a>
+						</div>
+					-->
+
+					</article>
+				</li>
+				<li>
+					<article>
+						<h4>
+							DONA IL TUO <br>
+							5 X MILLE <br>
+							A ITALIA CHE CAMBIA
+						</h4>
+
+						<div class='description'>
+							<p>
+								Italia che Cambia racconta, mappa e mette in rete circa 2.000 realtà del cambiamento italiane. Con il nostro lavoro diamo voce ad un Paese diverso, che collabora al posto di competere, che mette al centro la persona e i suoi bisogni, che si prende cura delle relazioni e degli ecosistemi in cui esse sono inserite.
+							</p>
+							<p>
+								Anche tu puoi partecipare a questo cambiamento e far sì che diventi inarrestabile!
+							</p>
+							<p>
+								Contribuisci col tuo 5 x mille!
+							</p>
+						</div>
+
+						<!--
+						<div class="cta">
+							<a href='' target='_blank'>ISCRIVITI ADESSO</a>
+						</div>
+					-->
+
+					</article>
+				</li>
+				<li>
+					<article>
+						<h4>
+							ADOTTA <br>
+							UN ARTICOLO
+						</h4>
+
+						<div class='description'>
+							<p>
+								Partecipando con l’1% del tuo stipendio, qualunque esso sia, contribuirai a sostenere le attività di Italia che Cambia orientate alla costruzione di un nuovo immaginario del nostro Paese, al racconto, mappatura e messa in rete dei processi in atto e realtà concrete del cambiamento italiano. <br>
+								Grazie al tuo contributo, continueremo a produrre contenuti originali per ispirare attraverso le storie del cambiamento, la mappatura delle realtà significative, per vedere tutto quel che già c’è in Italia e a organizzare incontri sul territorio per creare nuove relazioni.
+							</p>
+							<p>
+								Collaborando possiamo davvero cambiare le cose!
+							</p>
+						</div>
+
+						<!--
+						<div class="cta">
+							<a href='' target='_blank'>ISCRIVITI ADESSO</a>
+						</div>
+					-->
+
+					</article>
+				</li>
+				<li>
+					<article>
+						<h4>
+							SOSTIENICI <br>
+							CON UNA DONAZIONE
+						</h4>
+
+						<div class='description'>
+							<p>
+								Italia che Cambia racconta, mappa e mette in rete circa 2.000 realtà del cambiamento italiane. Con il nostro lavoro diamo voce ad un Paese diverso, che collabora al posto di competere, che mette al centro la persona e i suoi bisogni, che si prende cura delle relazioni e degli ecosistemi in cui esse sono inserite.
+							</p>
+							<p>
+								Anche tu puoi partecipare a questo cambiamento e far sì che diventi inarrestabile!
+							</p>
+							<p>
+								Contribuisci col tuo 5 x mille!
+							</p>
+						</div>
+						<!--
+						<div class="cta">
+							<a href='' target='_blank'>ISCRIVITI ADESSO</a>
+						</div>
+					-->
+					</article>
+				</li>
+			</ul>
+		</div>
+	</section>
+</main>
+
+<?php get_footer(); ?>
