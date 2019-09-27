ef1296eed7f1e34472c9fdf052b366061c23ce14
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-09-27 23:12:39 +0200

    Impostato colonne bootstrap (5 colonne per riga)

--- category.php
+++ category.php
@@ -136,65 +136,67 @@
     if( $loop->have_posts() ) :
       /* Eseguo qualcosa se ho post nel loop */
       ?>
-      <div class="grid">
+      <div class="row">
       <?php
       while( $loop->have_posts() ) : $loop->the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <a href='<?php the_permalink(); ?>'>
-            <?php
-            if ($Cat1 == $ParentCat1) { ?>
-              <div class='category'>
-                <span><?php the_time('j M') ?></span>
-                <span>
-                  <?php
-                    if (in_category('documentari')) {
-                      echo 'I documentari';
-                    } elseif (in_category('le-storie')) {
-                      echo 'Le storie';
-                    }elseif (in_category('meme')) {
-                      echo 'I meme';
-                    }elseif (in_category('rubriche')) {
-                      echo 'Le rubriche';
-                    }elseif (in_category('salute-che-cambia')) {
-                      echo 'Salute';
-                    }elseif (in_category('articoli')) {
-                      echo 'Gli Articoli';
-                    }
-                  ?>
-                </span>
-              </div>
-            <?php } ?>
-            <!-- Immagine in evidenza -->
-            <figure>
+        <div class="col-xl-5ths col-lg-3 col-md-4 col-sm-6">
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <a href='<?php the_permalink(); ?>'>
               <?php
-                if ($Cat1 != "nostri-libri"){
-                  the_post_thumbnail('icc_category', array('class' => 'img-res','alt' => get_the_title()));
-                } else {
-                  the_post_thumbnail('icc_libri', array('class' => 'img-res','alt' => get_the_title()));
+              if ($Cat1 == $ParentCat1) { ?>
+                <div class='category'>
+                  <span><?php the_time('j M') ?></span>
+                  <span>
+                    <?php
+                      if (in_category('documentari')) {
+                        echo 'I documentari';
+                      } elseif (in_category('le-storie')) {
+                        echo 'Le storie';
+                      }elseif (in_category('meme')) {
+                        echo 'I meme';
+                      }elseif (in_category('rubriche')) {
+                        echo 'Le rubriche';
+                      }elseif (in_category('salute-che-cambia')) {
+                        echo 'Salute';
+                      }elseif (in_category('articoli')) {
+                        echo 'Gli Articoli';
+                      }
+                    ?>
+                  </span>
+                </div>
+              <?php } ?>
+              <!-- Immagine in evidenza -->
+              <figure>
+                <?php
+                  if ($Cat1 != "nostri-libri"){
+                    the_post_thumbnail('icc_category', array('class' => 'img-fluid','alt' => get_the_title()));
+                  } else {
+                    the_post_thumbnail('icc_libri', array('class' => 'img-fluid','alt' => get_the_title()));
+                  }
+                ?>
+              </figure>
+              <!-- Autore o autori -->
+              <?php
+              if ($Cat1 != "nostri-libri"){
+                echo "<div class='autore'>Scritto da <b>".get_the_author();
+                /* controllo se esiste un secondo autore */
+                if( !empty (get_post_meta( get_the_ID(), 'SecondoAutore',true))){
+                  echo " e ". get_post_meta( get_the_ID(), 'SecondoAutore',true);
                 }
-              ?>
-            </figure>
-            <!-- Autore o autori -->
-            <?php
-            if ($Cat1 != "nostri-libri"){
-              echo "<div class='autore'>Scritto da <b>".get_the_author();
-              /* controllo se esiste un secondo autore */
-              if( !empty (get_post_meta( get_the_ID(), 'SecondoAutore',true))){
-                echo " e ". get_post_meta( get_the_ID(), 'SecondoAutore',true);
+                echo "</b></div>";
               }
-              echo "</b></div>";
-            }
-            ?>
-            <!-- Titolo -->
-            <div class='title'>
-              <h3><?php the_title(); ?></h3>
-            </div>
+              ?>
+              <!-- Titolo -->
+              <div class='title'>
+                <h3><?php the_title(); ?></h3>
+              </div>
 
-              <?php the_excerpt();?>
+                <?php the_excerpt();?>
 
-            <div class='cta'>LEGGI DI PIÙ</div>
-          </a>
-        </article>
+              <div class='cta'>LEGGI DI PIÙ</div>
+            </a>
+          </article>
+        </div>
         <?php
       endwhile;
         ?>
