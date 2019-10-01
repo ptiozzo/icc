5c8c52cf373ca706aa2e25fa8fbb4ae901e29ccc
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-01 21:52:12 +0200

    Creato template per pagine contatti

---
+++ template-casentinocontattaci.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Casentino contattaci
+*/
+?>
+<?php get_header(); ?>
+<main class="piemonte-che-cambia">
+  <?php get_template_part('menu','casentino'); ?>
+</main>
+<div class="content-no-sidebar" style="margin-top: 50px;">
+
+                <?php the_content();?>
+
+</div><!-- .content 	-->
+<!-- carico footer -->
+<?php get_footer(); ?>
---
+++ template-piemontecontattaci.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Piemonte contattaci
+*/
+?>
+<?php get_header(); ?>
+<main class="piemonte-che-cambia">
+  <?php get_template_part('menu','piemonte'); ?>
+</main>
+<div class="content-no-sidebar" style="margin-top: 50px;">
+
+                <?php the_content();?>
+
+</div><!-- .content 	-->
+<!-- carico footer -->
+<?php get_footer(); ?>
