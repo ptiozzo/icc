894c94076a1031b8abe3dbfb8a11810b160c8ad2
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-01 22:28:01 +0200

    FIX Menu regionali

--- menu-casentino.php
+++ menu-casentino.php
@@ -1,7 +1,7 @@
 <section class="page-head">
 
-  <nav class="navbar navbar-expand-lg navbar-white">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-dark navbar-expand-xl">
+    <button class="navbar-toggler" style="border:none;" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
    <a class="navbar-brand text-white" href="#">
@@ -21,27 +21,5 @@
      'walker'          => new bs4navwalker()
    ]);
    ?>
-</nav>
-
-<div class='right-section'>
-    <ul class='actions'>
-      <li class='border'>
-        <a href=''>
-          <figure>
-            <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/signup.svg' alt='' title=''>
-          </figure>
-          <span>Registrati</span>
-        </a>
-      </li>
-      <li class='border'>
-        <a href=''>
-          <figure>
-            <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/signin.svg' alt='' title=''>
-          </figure>
-          <span>Accedi</span>
-        </a>
-      </li>
-    </ul>
-  </div>
-
+  </nav>
 </section>
--- menu-piemonte.php
+++ menu-piemonte.php
@@ -1,7 +1,7 @@
 <section class="page-head">
 
-  <nav class="navbar navbar-expand-lg navbar-white ">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-dark navbar-expand-xl">
+    <button class="navbar-toggler" style="border:none;" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
    <a class="navbar-brand text-white" href="#"><img src='<?php echo get_template_directory_uri();?>/assets/img/modules/piemonte/piemonte.svg' alt='' title=''>
@@ -22,26 +22,5 @@
    ]);
    ?>
 </nav>
-
-<div class='right-section'>
-    <ul class='actions'>
-      <li class='border'>
-        <a href=''>
-          <figure>
-            <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/signup.svg' alt='' title=''>
-          </figure>
-          <span>Registrati</span>
-        </a>
-      </li>
-      <li class='border'>
-        <a href=''>
-          <figure>
-            <img src='<?php echo get_template_directory_uri();?>/assets/img/icons/signin.svg' alt='' title=''>
-          </figure>
-          <span>Accedi</span>
-        </a>
-      </li>
-    </ul>
-  </div>
 
 </section>
