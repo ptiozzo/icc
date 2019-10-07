83c7cb36777dd0167678130e987fe0df8c4ad2b3
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-07 22:12:30 +0200

    Creato template per mappa da collegare ad una pagina

---
+++ template-mappa.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Mappa
+*/
+?>
+<?php get_header(); ?>
+<main class="piemonte-che-cambia">
+  <?php get_template_part('menu','piemonte'); ?>
+</main>
+
+
+
+
+<!-- CONTENUTO -->
+
+
+
+
+<!-- carico footer -->
+<?php get_footer(); ?>
