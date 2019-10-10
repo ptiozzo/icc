568bb49502e3687cbe16e82d799f531554103a7c
Paolo <paolo@CP-13186.local>
2019-10-10 07:42:18 +0200

    Creata pagina per archive

--- index.php
+++ index.php
@@ -1,23 +1,95 @@
-<?php get_header();?>
-<div class="content">
-  <?php if( have_posts() ) : ?>
-      <h2>INDEX.PHP</h2>
-    <?php
-      while(have_posts() ) : the_post();
-  ?>
+<?php get_header(); ?>
+<div class="content-no-sidebar pt-2">
+  <div class="category-<?php echo get_term_by('name', single_cat_title('',false), 'category')->slug; ?> clearfix">
+<?php
 
-  <a href="<?php echo the_permalink();?>"><?php echo get_the_title(); ?></a><br />
+    if( have_posts() ) :
+      if (is_archive()){
+        the_archive_title('<h1 class="archive-title">', '</h1>');
+      }
+      ?>
+      <div class="row">
+      <?php
+      while( have_posts() ) : the_post(); ?>
+        <div class="col-xl-5ths col-lg-3 col-md-4 col-sm-6">
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <a href='<?php the_permalink(); ?>'>
+              <?php
+              if ($Cat1 == $ParentCat1) { ?>
+                <div class='category'>
+                  <span><?php the_time('j M') ?></span>
+                  <span>
+                    <?php
+                      if (in_category('documentari')) {
+                        echo 'I documentari';
+                      } elseif (in_category('le-storie')) {
+                        echo 'Le storie';
+                      }elseif (in_category('meme')) {
+                        echo 'I meme';
+                      }elseif (in_category('rubriche')) {
+                        echo 'Le rubriche';
+                      }elseif (in_category('salute-che-cambia')) {
+                        echo 'Salute';
+                      }elseif (in_category('articoli')) {
+                        echo 'Gli Articoli';
+                      }
+                    ?>
+                  </span>
+                </div>
+              <?php } ?>
+              <!-- Immagine in evidenza -->
+              <figure>
+                <?php
 
-  <?php
+                    if ( has_post_thumbnail() ) {
+      								the_post_thumbnail('icc_ultimenewshome', array('class' => 'img-fluid card-img-top mx-auto d-block p-1','alt' => get_the_title()));
+      							}
+      							else{
+      								echo '<img class="img-fluid card-img-top mx-auto d-block p-1" src="'.catch_that_image().'" />';
+      							}
+
+                ?>
+              </figure>
+              <!-- Autore o autori -->
+              <?php
+              if ($Cat1 != "nostri-libri"){
+                echo "<div class='autore'>Scritto da <b>".get_the_author();
+                /* controllo se esiste un secondo autore */
+                if( !empty (get_post_meta( get_the_ID(), 'SecondoAutore',true))){
+                  echo " e ". get_post_meta( get_the_ID(), 'SecondoAutore',true);
+                }
+                echo "</b></div>";
+              }
+              ?>
+              <!-- Titolo -->
+              <div class='title'>
+                <h3><?php the_title(); ?></h3>
+              </div>
+
+                <?php the_excerpt();?>
+
+              <div class='cta'>LEGGI DI PIÙ</div>
+            </a>
+          </article>
+        </div>
+        <?php
       endwhile;
-  else:
-  ?>
+        ?>
+      </div> <!-- .grid -->
+        <!-- paginazione -->
 
-  <p>Non ho trovato nulla</p>
+      <?php echo bootstrap_pagination($loop); ?>
 
-  <?php
-  endif;
+        <?php
+      else:
+        ?>
+          <p>Spiacente, ma la tua ricerca non ha prodotto nessun risultato</p>
+        <?php
+      endif;
+    wp_reset_query();
   ?>
-</div> <!-- .content 	-->
-<?php get_sidebar(); ?>
+  <!-- fine loop -->
+</div><!-- .category -->
+</div><!-- .content 	-->
+<!-- carico footer -->
 <?php get_footer(); ?>
