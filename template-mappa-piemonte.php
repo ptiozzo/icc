932df5bd70cad2b125ca241840a1a74d5323a72e
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-14 23:31:20 +0200

    Creazione mappa piemonte

---
+++ template-mappa-piemonte.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Mappa Piemonte
+*/
+?>
+<?php get_header(); ?>
+<main class="piemonte-che-cambia">
+  <?php get_template_part('menu','piemonte'); ?>
+</main>
+
+
+
+
+<!-- CONTENUTO -->
+<iframe style="border: 0px; width:100%; height: 500px;" src=""></iframe>
+
+
+
+<!-- carico footer -->
+<?php get_footer(); ?>
