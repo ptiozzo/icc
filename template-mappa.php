4d0871dd9238771d8ee3fda40422eab578901e43
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-15 20:52:55 +0200

    Mappa

--- template-mappa.php
+++ template-mappa.php
@@ -4,17 +4,9 @@ Template Name: Mappa
 */
 ?>
 <?php get_header(); ?>
-<main class="piemonte-che-cambia">
-  <?php get_template_part('menu','piemonte'); ?>
-</main>
-
-
-
 
 <!-- CONTENUTO -->
-
-
-
+<iframe style="border: 0px; width:100%; height: 80vh;" src="http://api.pianetafuturo.it/widget/map/std2.php?a=3&tagoverride=1&sidebar=1"></iframe>
 
 <!-- carico footer -->
 <?php get_footer(); ?>
