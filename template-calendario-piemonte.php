4a013af96d069d45aa802233224eb7ed88abfa8c
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-16 19:01:23 +0200

    Template piemonte

---
+++ template-calendario-piemonte.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Calendario Piemonte
+*/
+?>
+<?php get_header(); ?>
+<main class="piemonte-che-cambia">
+  <?php get_template_part('menu','piemonte'); ?>
+</main>
+
+
+
+
+<!-- CONTENUTO -->
+<div style="margin-top: 80px; padding: 20px;"><iframe style="border: 0px; width:100%; height: 1000px;" src="http://api.pianetafuturo.it/widget/calendar/calendar.php?a=2"></iframe></div>
+
+
+
+<!-- carico footer -->
+<?php get_footer(); ?>
--- template-mappa-piemonte.php
+++ template-mappa-piemonte.php
@@ -12,7 +12,7 @@ Template Name: Mappa Piemonte
 
 
 <!-- CONTENUTO -->
-<iframe style="border: 0px; width:100%; height: 500px;" src=""></iframe>
+<div style="margin-top: 80px;"><iframe style="border: 0px; width:100%; height: 80vh;" src="http://api.pianetafuturo.it/widget/map/std2.php?a=2&tagoverride=1&sidebar=1&nored=1"></iframe></div>
 
 
 
