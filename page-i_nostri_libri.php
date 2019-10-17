61db4b70f532a707243e83d69a4b78c1b594cce4
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-17 21:51:47 +0200

    Pagina i nostri libri

--- page-i_nostri_libri.php
+++ page-i_nostri_libri.php
@@ -16,7 +16,7 @@ echo "Non ho trovato nulla";
 endif;
 ?>
 
-<div class="grid">
+<div class="row">
 
 <?php
 //loop i nostri libri
@@ -29,11 +29,46 @@ $custom_query = new WP_Query( $custom_query_args );
 ?>
 
   <?php if ( $custom_query->have_posts() ) : while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <?php the_post_thumbnail('icc_category', array('class' => 'img-res','alt' => get_the_title())); ?>
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <?php the_excerpt();?>
-    </article>
+    <div class="col-xl-5ths col-lg-3 col-md-4 col-sm-6">
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <a href='<?php the_permalink(); ?>'>
+          <!-- Immagine in evidenza -->
+          <figure>
+            <?php
+              if ($Cat1 != "nostri-libri"){
+                if ( has_post_thumbnail() ) {
+                  the_post_thumbnail('icc_ultimenewshome', array('class' => 'img-fluid card-img-top mx-auto d-block p-1','alt' => get_the_title()));
+                }
+                else{
+                  echo '<img class="img-fluid card-img-top mx-auto d-block p-1" src="'.catch_that_image().'" />';
+                }
+              } else {
+                the_post_thumbnail('icc_libri', array('class' => 'img-fluid','alt' => get_the_title()));
+              }
+            ?>
+          </figure>
+          <!-- Autore o autori -->
+          <?php
+          if ($Cat1 != "nostri-libri"){
+            echo "<div class='autore'>Scritto da <b>".get_the_author();
+            /* controllo se esiste un secondo autore */
+            if( !empty (get_post_meta( get_the_ID(), 'SecondoAutore',true))){
+              echo " e ". get_post_meta( get_the_ID(), 'SecondoAutore',true);
+            }
+            echo "</b></div>";
+          }
+          ?>
+          <!-- Titolo -->
+          <div class='title'>
+            <h3><?php the_title(); ?></h3>
+          </div>
+
+            <?php the_excerpt();?>
+
+          <div class='cta'>LEGGI DI PIÙ</div>
+        </a>
+      </article>
+    </div>
   <?php endwhile; endif; wp_reset_postdata(); ?>
 </div> <!-- .grid 	-->
 </div> <!-- .content 	-->
