0f07d4053bd9e4567f0c63323d0d4640d443b9c4
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-21 21:57:54 +0200

    Aggiunte istruzioni per aggiunta nuova regione

--- adm/theme-istruction.php
+++ adm/theme-istruction.php
@@ -1,37 +1,19 @@
 <div class="wrap">
   <h2>Istruzioni per template</h2>
   <p>Ecco alcune note per l'utilizzo corretto del sito</p>
-  q<h2>Home page</h2>
-  <p>L'home page carica:</p>
-  <ul>
-    <li>6 post con categoria "le storie"</li>
-    <li>3 post con categoria "rassegna stampa"</li>
-    <li>Ultimi 10 post TRANNE quelli in categoria rassegna stampa</li>
-    <li>3 post con "Libri"</li>
-  </ul>
-  <h2>Menu</h2>
-  <p>Sono stati creati 8 menu:</p>
-  <ul>
-    <li>Menu principale (Quello in alto tra logo e cerca)</li>
-    <li>Menu social (Per FB, TW e YT)</li>
-    <li>Menu footer (Relativo a cookie, privacy, etc)</li>
-    <li>Menu i nostri contenuti (A scomparsa con Articoli, meme, etc)</li>
-    <li>Menu regioni (piemonte, casentino, berlin, brandeburg)</li>
-    <li>Menu piemonte (quando si entra nella categoria piemonte)</li>
-    <li>Menu casentino (quando si entra nella categoria casentino)</li>
-  </ul>
-  <p>I menu sono modificabili dalla parte di amministrazione, ma prego di chiedere prima di procedere</p>
-  <h2>I nostri contenuti</h2>
+
+
+
+  <h2>Aggiungere nuova regione</h2>
   <p>
-    La pagina caricata per I nostri contenuti prende tutti gli articoli con categoria
-    I nostri contenuti e Tematiche o categorie "figlie" di queste con almeno 1 articolo associato.<br />
-    Tutti i post devono avere la doppia categoria (i nostri contenuti e tematiche - o figlie) altrimenti i post
-    non vengono visualzzati quando si applica il filtro.
+    <ul>
+      <li>Creare la categoria "regione che cambia"</li>
+      <li>Creare il template della regione ed impostare il $catPage con lo slug della categoria</li>
+      <li>Creare la pagina "regione che cambia" associando il template sopra creato e facendo attenzione che lo slug sia solo "regione"</li>
+      <li>Aggiungere la pagina al menu "regioni"</li>
+      <li>Crare il menu personalizzato nel function.php</li>
+      <li>Creare il nuovo menu dal backend di WP</li>
+      <li>Aggiungere le voci desiderate al menu'</li>
+      <li>Creare la pagina sul template con il formato desiderato</li>
+    </ul>
   </p>
-  <h2>Campagne tematiche e Nostri libri</h2>
-  <p>
-    Le campagne tematiche sono dei Costum Post Type.</br>
-    Questi vengono gestiti come dei normali post, solo che vengono separati dagli altri post nella visualizzazione
-    ed hanno bisogno di avere una personalizzazione del codice per venir visualizzati.
-  </p>
-</div>
