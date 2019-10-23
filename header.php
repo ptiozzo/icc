699073862e0a0a0f4b308da83fe39b67efde5f74
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-23 21:14:03 +0200

    Fix grafico del title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>
-			<?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
+			<?php bloginfo('name'); ?> <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
 		</title>
 		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 		<meta name="title" content="<?php bloginfo( 'name' ); ?>">
