ebdd057b9bf4dded4a9bf3c140e1ca06cc78302a
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-10-25 23:30:25 +0200

    Creata pagina 404

--- 404.php
+++ 404.php
@@ -1,4 +1,24 @@
 <?php get_header(); ?>
-404
-<?php get_sidebar(); ?>
+<div class="container-fluid cerca">
+  <div class="category-<?php echo get_term_by('name', single_cat_title('',false), 'category')->slug; ?> clearfix">
+    <h1 class="pt-4">OPS!</h1>
+    <p>La pagina cercata non esiste, prova ad effettuare un'ulteriore ricerca, grazie</p>
+    <p>La pagina è stata segnalata alla redazione!</p>
+    <?php
+    $to = 'ptiozzo@me.com';
+    $subject = '[ICC] 404';
+    $body = "E' stata trovata una pagina 404 <br>". get_pagenum_link();
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+
+    wp_mail( $to, $subject, $body, $headers );
+     ?>
+    <div class="contenuti_header text-center ">
+      <form class="mb-2" method="post" action="/cerca/">
+            <input class="mb-2" type="text" name="termine-cercato" value="<?php echo $searchterm; ?>">
+            <input name="submit_button" type="Submit" value="Cerca">
+      </form>
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
