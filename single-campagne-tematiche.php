150b07ff1e1e74bffb2d40f95f889c3f1e296793
Paolo <paolo@CP-13186.local>
2019-11-11 14:07:59 +0100

    Rimossa immagine testata

--- single-campagne-tematiche.php
+++ single-campagne-tematiche.php
@@ -3,25 +3,7 @@
 	<?php if (have_posts()) :?><?php while(have_posts()) : the_post(); ?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<!-- Thumbnail o video youtube -->
-			<div class="single__thumbnail">
-				<?php
-				if( !empty (get_post_meta( get_the_ID(), 'YouTubeLink',true))){
-					?>
-					<figure class="embed-responsive embed-responsive-16by9">
-						<iframe width="800" height="480" src="https://www.youtube.com/embed/<?php echo linkifyYouTubeURLs(get_post_meta( get_the_ID(), 'YouTubeLink',true));?>?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
-					</figure>
-					<?php
-				}
-				elseif ( has_post_thumbnail() ) {
-					the_post_thumbnail('icc_ultimenewshome', array('class' => 'img-fluid card-img-top mx-auto d-block p-1','alt' => get_the_title()));
-				}
-				else{
-					echo '<img class="img-fluid card-img-top mx-auto d-block p-1" src="'.catch_that_image().'" />';
-				}
-
-				?>
-			</div>
+			
 
 			<!-- TAG -->
 			<div class="single__head">
