ddc641b8febc5564496a002294de689810ed93ce
Paolo <paolo@CP-13186.local>
2019-11-13 07:47:08 +0100

    Aggiunto controllo se pagina figlia di liguria

--- page.php
+++ page.php
@@ -1,9 +1,16 @@
 <?php get_header(); ?>
+<?php
+  //controllo le pagina è figlia di Liguria
+  if ( $post->post_parent == '58464' ) {
+    get_template_part('menu','liguria');
+  }
+?>
 <div class="container pt-4">
   <?php if( have_posts() ) : ?>
     <?php
       while(have_posts() ) : the_post();
   ?>
+
 
   <h1><?php echo get_the_title(); ?></h1>
 
