dc60cb3acad9b3098d78e67f13cb313dadfbd072
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-11-18 20:41:04 +0100

    Aggiunto widget per favola

--- inc/widget.php
+++ inc/widget.php
@@ -51,9 +51,63 @@ class icc_Widget_CampagneTematiche extends WP_Widget {
 
 }
 
+
+class icc_Widget_UnaFavolaPuoFare extends WP_Widget {
+
+
+  // Set up the widget name and description.
+  public function __construct() {
+    $widget_options = array( 'classname' => 'icc_Widget_UnaFavolaPuoFare', 'description' => 'Visualizza ultima favola' );
+    parent::__construct( 'Widget_UnaFavolaPuoFare', 'Visualizza ultima favola', $widget_options );
+  }
+
+
+  // Create the widget output.
+  public function widget( $args, $instance ) {
+    $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];
+
+    $custom_query_args = array(
+    'category_name' => 'una-favola-puo-fare',
+    'posts_per_page'=> 1,
+    );
+
+    $custom_query = new WP_Query( $custom_query_args );
+     if ( $custom_query->have_posts() ) :
+       while ( $custom_query->have_posts() ) : $custom_query->the_post();?>
+        <a href="<?php the_permalink(); ?>">
+          <figure>
+            <?php the_post_thumbnail('icc_sidebar', array('class' => 'img-res','alt' => get_the_title())); ?>
+          </figure>
+          <h4><?php the_title(); ?></h4>
+          <?php the_excerpt();?>
+        </a>
+      <?php endwhile; endif; wp_reset_postdata(); ?>
+    <?php echo $args['after_widget'];
+  }
+  // Create the admin area widget settings form.
+  public function form( $instance ) {
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+    </p><?php
+  }
+
+
+  // Apply settings to the widget instance.
+  public function update( $new_instance, $old_instance ) {
+    $instance = $old_instance;
+    $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+    return $instance;
+  }
+
+}
+
 // Register the widget.
 function jpen_register_example_widget() {
   register_widget( 'icc_Widget_CampagneTematiche' );
+  register_widget( 'icc_Widget_UnaFavolaPuoFare' );
 }
 add_action( 'widgets_init', 'jpen_register_example_widget' );
 
