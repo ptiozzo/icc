63b2b9e50c4b34e93ce54793fc7c4a04cc046129
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2019-11-26 22:35:20 +0100

    Aggiunt riferimenti ad ultimi aggiornamenti dati da pianetafuturo

--- adm/theme.php
+++ adm/theme.php
@@ -1,4 +1,11 @@
 <div class="wrap">
   <h2>Tema</h2>
   <p>Tema costruito per il sito italiachecambia.org</p>
+  <h2>Aggiornamenti dati tramite API di pianetafuturo</h2>
+  <p><b>Mappa per regione</b></p>
+  <p><?php echo date("d/m/Y h:i:s A T",get_option('icc_mappa_regioni_lastupdate')); ?></p>
+  <p><b>Mappa per reti</b></p>
+  <p><?php echo date("d/m/Y h:i:s A T",get_option('icc_mappa_reti_lastupdate')); ?></p>
+  <p><b>Ultime realtà mappate (home)</b></p>
+  <p><?php echo date("d/m/Y h:i:s A T",get_option('icc_realta_mappate_lastupdate')); ?></p>
 </div>
