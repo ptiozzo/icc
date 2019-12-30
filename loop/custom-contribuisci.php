75e1f1d4fa0f8cad87d64567b24891da94d6acc2
Paolo <paolo@EDP4YOUs-MacBook-Air.local>
2019-12-30 16:18:56 +0100

    Fix colore bottone

--- loop/custom-contribuisci.php
+++ loop/custom-contribuisci.php
@@ -6,7 +6,7 @@
     <p>Il contributo di ogni lettore, piccolo o grande, ha grande valore ed è essenziale per proteggere l'editoria indipendente.</p>
     <p>Se ne hai la possibilità contribuisci a Italia che Cambia oggi anche con un piccolo contributo.</p>
   </div>
-  <div class="col-auto d-flex align-items-end"><a class="btn btn-dark mb-3" href="/sostienici/">Contribuisci</a></div>
+  <div class="col-auto d-flex align-items-end"><a class="btn btn-dark mb-3 text-white" href="/sostienici/">Contribuisci</a></div>
   <div class="col-auto"><button class="btn btn-dark mt-1 text-black" type="button" data-toggle="collapse" data-target="#contribuisci" aria-expanded="false" aria-controls="collapsContribuisci">X</a></div>
 </div>
 <script>
