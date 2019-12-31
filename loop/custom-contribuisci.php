a6538c09ce9bcaf61a7f7bf28bc624852a5c22b6
Paolo <paolo@EDP4YOUs-MacBook-Air.local>
2019-12-31 15:18:43 +0100

    Fix mobile

--- loop/custom-contribuisci.php
+++ loop/custom-contribuisci.php
@@ -1,18 +1,18 @@
-<div class="row contribuisci collapse <?php if($_COOKIE['contribuisci']!= 'close'){echo "show";} ?> mx-0" id="contribuisci">
-  <div class="col-7 offset-lg-2 p-2 font-weight-light">
-    <p>Sempre più persone, come te, comprendono la necessità di un giornalismo indipendente e costruttivo.<br>
+<div class="row contribuisci collapse <?php if($_COOKIE['contribuisci']!= 'close2'){echo "show";} ?> mx-0" id="contribuisci">
+  <div class="col-12 col-md-7 offset-lg-2 p-2 font-weight-light">
+    <p class="d-none d-md-block">Sempre più persone, come te, comprendono la necessità di un giornalismo indipendente e costruttivo.<br>
     <b>Italia che Cambia</b> è da sempre impegnata nella diffusione di notizie che contribuiscano a costruire un nuovo immaginario sul nostro Paese, fornendo esempi concreti per la transizione verso un mondo migliore.
     Abbiamo scelto di mantenere le nostre notizie gratuite e disponibili per tutti, riconoscendo l’importanza che ciascuno di noi abbia accesso a un giornalismo accurato e costruttivo.</p>
     <p>Il contributo di ogni lettore, piccolo o grande, ha grande valore ed è essenziale per proteggere l'editoria indipendente.</p>
-    <p>Se ne hai la possibilità contribuisci a Italia che Cambia oggi anche con un piccolo contributo.</p>
+    <p class="d-none d-md-block">Se ne hai la possibilità contribuisci a Italia che Cambia oggi anche con un piccolo contributo.</p>
   </div>
   <div class="col-auto d-flex align-items-end"><a class="btn btn-dark mb-3 text-white" href="/sostienici/">Contribuisci</a></div>
-  <div class="col-auto"><button class="btn btn-dark mt-1 text-black" type="button" data-toggle="collapse" data-target="#contribuisci" aria-expanded="false" aria-controls="collapsContribuisci">X</a></div>
+  <div class="col-auto"><button class="btn btn-dark mt-md-1 text-black" type="button" data-toggle="collapse" data-target="#contribuisci" aria-expanded="false" aria-controls="collapsContribuisci">X</a></div>
 </div>
 <script>
 $('#contribuisci').on('hidden.bs.collapse', function () {
   var date = new Date();
-  date.setTime(date.getTime()+(30*24*60*60*1000));
+  date.setTime(date.getTime()+(7*24*60*60*1000));
   document.cookie = "contribuisci=close; expires=" + date.toGMTString() + ";path=/";
 })
 </script>
