586bcfc2f3003a8e68d0c6f803f0a39a7d733d86
Paolo <paolo@EDP4YOUs-Air.intranet.edp4you.it>
2020-01-09 11:10:21 +0100

    Fix to contribuisci

--- loop/custom-contribuisci.php
+++ loop/custom-contribuisci.php
@@ -1,4 +1,4 @@
-<div class="row contribuisci collapse <?php if($_COOKIE['contribuisci']!= 'close'){echo "show";} ?> mx-0" id="contribuisci">
+<div class="row contribuisci collapse <?php if($_COOKIE['contribuisci']!= 'close2'){echo "show";} ?> mx-0" id="contribuisci">
   <div class="col-12 col-md-7 offset-lg-2 p-2 font-weight-light">
     <p class="d-none d-md-block">Sempre più persone, come te, comprendono la necessità di un giornalismo indipendente e costruttivo.<br>
     <b>Italia che Cambia</b> è da sempre impegnata nella diffusione di notizie che contribuiscano a costruire un nuovo immaginario sul nostro Paese, fornendo esempi concreti per la transizione verso un mondo migliore.
@@ -6,8 +6,8 @@
     <p>Il contributo di ogni lettore, piccolo o grande, ha grande valore ed è essenziale per proteggere l'editoria indipendente.</p>
     <p class="d-none d-md-block">Se ne hai la possibilità contribuisci a Italia che Cambia oggi anche con un piccolo contributo.</p>
   </div>
-  <div class="col-auto d-flex align-items-end"><a class="btn btn-dark mb-3 text-white" href="/sostienici/">Contribuisci</a></div>
-  <div class="col-auto"><button class="btn btn-dark mt-md-1 text-black" type="button" data-toggle="collapse" data-target="#contribuisci" aria-expanded="false" aria-controls="collapsContribuisci">X</a></div>
+  <div class="col-auto d-flex align-items-end"><a class="btn btn-dark mb-3 text-white rounded-pill" href="/sostienici/">Contribuisci</a></div>
+  <div class="col-auto ml-auto"><button class="btn btn-outline-dark my-3 rounded-pill" type="button" data-toggle="collapse" data-target="#contribuisci" aria-expanded="false" aria-controls="collapsContribuisci">X</a></div>
 </div>
 <script>
 $('#contribuisci').on('hidden.bs.collapse', function () {
