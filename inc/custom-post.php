f7ab0c061a056ac45fad297fef52c3d0e8fa5eeb
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2020-01-14 22:27:35 +0100

    Aggiunto CPT contenuti speciali

--- inc/custom-post.php
+++ inc/custom-post.php
@@ -130,4 +130,46 @@ function icc_rassegna_stampa_custom_post() {
 // Inizializzo la funzione
 add_action( 'init', 'icc_rassegna_stampa_custom_post');
 
+/*  Custom post type Contenuti speciali
+/* ------------------------------------ */
+function icc_contenuti_speciali_custom_post() {
+    // creo e registro il custom post type
+    register_post_type('contenuti-speciali', /* nome del custom post type */
+        // definisco le varie etichette da mostrare nei menù
+        array('labels' => array(
+            'name' => 'Contenuti speciali', /* nome, al plurale, dell'etichetta del post type. */
+            'singular_name' => 'Contenuti speciali', /* nome, al singolare, dell'etichetta del post type. */
+            'all_items' => 'Tutti i Contenuti speciali', /* testo nei menu che indica tutti i contenuti del post type */
+            'add_new' => 'Aggiungi nuovo Contenuto speciale', /*testo del pulsante Aggiungi. */
+            'add_new_item' => 'Aggiungi Contenuto speciale', /* testo per il pulsante Aggiungi nuovo post type */
+            'edit_item' => 'Modifica', /*  testo modifica */
+            'new_item' => 'Nuovo', /* testo nuovo oggetto */
+            'view_item' => 'Visualizza', /* testo per visualizzare */
+            'search_items' => 'Cerca', /* testo per la ricerca*/
+            'not_found' =>  'Nessun Contenuto speciale trovato', /* testo se non trova nulla */
+            'not_found_in_trash' => 'Nessun Contenuto speciale trovato nel cestino', /* testo se non trova nulla nel cestino */
+            'parent_item_colon' => ''
+            ), /* fine dell'array delle etichette del menu */
+            'description' => 'Contenuti speciali', /* descrizione del post type */
+            'public' => true, /* definisce se il post type sia visibile sia da front-end che da back-end */
+            'publicly_queryable' => true, /* definisce se possono essere fatte query da front-end */
+            'exclude_from_search' => false, /* esclude (false) il post type dai risultati di ricerca */
+            'show_ui' => true, /* definisce se deve essere visualizzata l'interfaccia di default nel pannello di amministrazione */
+            'query_var' => true,
+            'menu_position' => 21, /* definisce l'ordine in cui comparire nel menù di amministrazione a sinistra */
+            'menu_icon' => 'dashicons-layout', /* imposta l'icona da usare nel menù per il posty type */
+            'rewrite'   => array( 'slug' => 'contenuto-speciale', 'with_front' => false ), /* specificare uno slug per leURL */
+						'show_in_rest' => 'true', /* abilita gutemberg come editor */
+            'has_archive' => true, /* definisci se abilitare la generazione di un archivio (tipo archive-cd.php) */
+            'capability_type' => 'post', /* definisci se si comporterà come un post o come una pagina */
+            'hierarchical' => false, /* definisci se potranno essere definiti elementi padri di altri */
+            /* la riga successiva definisce quali elementi verranno visualizzati nella schermata di creazione del post */
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+        ) /* fine delle opzioni */
+    ); /* fine della registrazione */
+
+}
+// Inizializzo la funzione
+add_action( 'init', 'icc_contenuti_speciali_custom_post');
+
 ?>
