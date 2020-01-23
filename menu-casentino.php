31033a6330e5d8af3f1f52de28ee82ec6d0ac61f
Paolo <paolo@EDP4YOUs-Air.intranet.edp4you.it>
2020-01-23 16:39:18 +0100

    Corretti link per "home" portali regionali

--- menu-casentino.php
+++ menu-casentino.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" style="border:none;" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand text-white" href="/casentino">
+    <a class="navbar-brand text-white" href="/casentino/">
      <img src='<?php echo get_template_directory_uri();?>/assets/img/modules/casentino/casentino.svg' class="d-none d-sm-inline-block" alt='' title=''>
      CASENTINO CHE CAMBIA
     </a>
--- menu-liguria.php
+++ menu-liguria.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" style="border:none;" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand text-white" href="/liguria">
+    <a class="navbar-brand text-white" href="/liguria/">
       <img src='<?php echo get_template_directory_uri();?>/assets/img/modules/liguria/liguria.svg' class="d-none d-sm-inline-block" alt='' title=''>
       LIGURIA CHE CAMBIA
    </a>
