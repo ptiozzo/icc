83d915156b269f37734a58ad519203f29401dcef
Paolo <paolo@EDP4YOUs-Air.intranet.edp4you.it>
2020-03-04 11:58:35 +0100

    Aggiunto sidebar su bacheca e calendario portali regionali

--- liguria/menu-liguria.php
+++ liguria/menu-liguria.php
@@ -76,3 +76,121 @@
     </div>
   </div>
 </div>
+
+<?php
+$argsLiguriaSegnalaProgetto = array(
+  'post_type' => 'contenuti-speciali',
+  'posts_per_page' => 1,
+  'tax_query' => array(
+    array(
+        'taxonomy'=> 'contenuti_speciali_filtri',
+        'field'   => 'slug',
+        'terms'		=> 'liguria-segnala-progetto',
+    ),
+  ),
+);
+$loopLiguriaSegnalaProgetto = new WP_Query( $argsLiguriaSegnalaProgetto );
+if($loopLiguriaSegnalaProgetto->have_posts()):
+ ?>
+<div class="modal fade" id="LiguriaSegnalaProgetto" tabindex="-1" role="dialog" aria-labelledby="LiguriaAccediTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="LiguriaAccediTitle">Segnala un progetto</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body pcc-pianfut">
+        <?php
+          while($loopLiguriaSegnalaProgetto->have_posts()) :  $loopLiguriaSegnalaProgetto->the_post();
+            the_content();
+          endwhile;
+          ?>
+        <a href="https://liguria.pianetafuturo.it">Vai a PianetaFuturo</a>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
+<?php
+$argsLiguriaSegnalaEvento = array(
+  'post_type' => 'contenuti-speciali',
+  'posts_per_page' => 1,
+  'tax_query' => array(
+    array(
+        'taxonomy'=> 'contenuti_speciali_filtri',
+        'field'   => 'slug',
+        'terms'		=> 'liguria-segnala-evento',
+    ),
+  ),
+);
+$loopLiguriaSegnalaEvento = new WP_Query( $argsLiguriaSegnalaEvento );
+if($loopLiguriaSegnalaEvento->have_posts()):
+ ?>
+<div class="modal fade" id="LiguriaSegnalaEvento" tabindex="-1" role="dialog" aria-labelledby="LiguriaAccediTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="LiguriaAccediTitle">Segnala un evento</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body pcc-pianfut">
+        <?php
+          while($loopLiguriaSegnalaEvento->have_posts()) :  $loopLiguriaSegnalaEvento->the_post();
+            the_content();
+          endwhile;
+          ?>
+        <a href="https://liguria.pianetafuturo.it">Vai a PianetaFuturo</a>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
+<?php
+$argsLiguriaScendiPiazza = array(
+  'post_type' => 'contenuti-speciali',
+  'posts_per_page' => 1,
+  'tax_query' => array(
+    array(
+        'taxonomy'=> 'contenuti_speciali_filtri',
+        'field'   => 'slug',
+        'terms'		=> 'liguria-scendi-piazza',
+    ),
+  ),
+);
+$loopLiguriaScendiPiazza = new WP_Query( $argsLiguriaScendiPiazza );
+if($loopLiguriaScendiPiazza->have_posts()):
+ ?>
+<div class="modal fade" id="LiguriaScendiPiazza" tabindex="-1" role="dialog" aria-labelledby="LiguriaAccediTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="LiguriaAccediTitle">Scendi in piazza</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body pcc-pianfut">
+        <?php
+          while($loopLiguriaScendiPiazza->have_posts()) :  $loopLiguriaScendiPiazza->the_post();
+            the_content();
+          endwhile;
+          ?>
+        <a href="https://liguria.pianetafuturo.it">Vai a PianetaFuturo</a>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
