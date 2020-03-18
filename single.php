cf396d7e00f7e91aca2084ae96344058429b153d
Paolo <paolo@EDP4YOUs-MacBook-Air.local>
2020-03-18 22:41:56 +0100

    Impostato contribuisci mobile/desktop

--- single.php
+++ single.php
@@ -150,22 +150,43 @@
 			</div>
 			<div class="single__contribuisci mb-2 position-relative">
 				<?php
-					$argsContribuisciSingle = array(
+				$argsContribuisciSingleDesktop = array(
 			    'post_type' => 'contenuti-speciali',
 			    'posts_per_page' => 1,
 			    'tax_query' => array(
 			      array(
 			          'taxonomy'=> 'contenuti_speciali_filtri',
 			          'field'   => 'slug',
-			          'terms'		=> 'contribuisci-singolo-articolo',
+			          'terms'		=> 'contribuisci-singolo-articolo-desktop',
 			      ),
 			    ),
 			  );
-			  $loopContribuisciSingle = new WP_Query( $argsContribuisciSingle );
-				if( $loopContribuisciSingle->have_posts()):
-					while( $loopContribuisciSingle->have_posts() ) : $loopContribuisciSingle->the_post();
+			  $loopContribuisciSingleDesktop = new WP_Query( $argsContribuisciSingleDesktop );
+
+				$argsContribuisciSingleMobile = array(
+			    'post_type' => 'contenuti-speciali',
+			    'posts_per_page' => 1,
+			    'tax_query' => array(
+			      array(
+			          'taxonomy'=> 'contenuti_speciali_filtri',
+			          'field'   => 'slug',
+			          'terms'		=> 'contribuisci-singolo-articolo-mobile',
+			      ),
+			    ),
+			  );
+			  $loopContribuisciSingleMobile = new WP_Query( $argsContribuisciSingleMobile );
+
+				if( $loopContribuisciSingleDesktop->have_posts() || $loopContribuisciSingleMobile->have_posts()):
+					while( $loopContribuisciSingleDesktop->have_posts() ) : $loopContribuisciSingleDesktop->the_post();
 					?>
-					<div class="m-2">
+					<div class="m-2 d-none d-md-block">
+					<?php the_content(); ?>
+					</div>
+					<?php
+					endwhile;
+					while( $loopContribuisciSingleMobile->have_posts() ) : $loopContribuisciSingleMobile->the_post();
+					?>
+					<div class="m-2 d-block d-md-none">
 					<?php the_content(); ?>
 					</div>
 					<?php
@@ -173,8 +194,8 @@
 				endif;
 				?>
 				<button type="button" class="btn btn-lg btn-block btn-warning">
-					Contribuisci adesso all'Italia che Cambia
-					<img src='<?php echo get_template_directory_uri();?>/assets/img/payment-methods.png'>
+					<b>Contribuisci adesso all'Italia che Cambia</b>
+					<img src='<?php echo get_template_directory_uri();?>/assets/img/payment-methods.png' class="ml-2">
 				</button>
 				<a href="/contribuisci" class="stretched-link"></a>
 			</div>
