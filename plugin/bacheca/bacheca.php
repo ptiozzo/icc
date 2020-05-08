46375ed428198cf1fca4c9720ff808983c6bbe4f
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2020-05-08 17:37:56 +0200

    Aggiunto single e archive

--- plugin/bacheca/bacheca.php
+++ plugin/bacheca/bacheca.php
@@ -21,5 +21,41 @@
    }
  }
 
+ add_filter('template_include', 'cercooffro_archive_template');
+
+ function cercooffro_archive_template( $template ) {
+   if ( is_post_type_archive('cerco-offro') ) {
+     $theme_files = array('archive-cerco-offro.php', 'archive-cerco-offro.php');
+     $exists_in_theme = locate_template($theme_files, false);
+     echo $exists_in_theme;
+     if ( $exists_in_theme != '' ) {
+       return $exists_in_theme;
+     } else {
+       return dirname( __FILE__ ) . '/archive-cerco-offro.php';
+     }
+   }
+   return $template;
+ }
+
+ add_filter('single_template', 'cercooffro_single_template');
+
+ function cercooffro_single_template( $template ) {
+   if ( get_post_type() == "cerco-offro" ) {
+     $theme_files = array('single-cerco-offro.php', 'single-cerco-offro.php');
+     $exists_in_theme = locate_template($theme_files, false);
+     echo $exists_in_theme;
+     if ( $exists_in_theme != '' ) {
+       return $exists_in_theme;
+     } else {
+       return dirname( __FILE__ ) . '/single-cerco-offro.php';
+     }
+   }
+   return $template;
+ }
+
+ function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
 
  ?>
