b8986a4124dda7404d4ffb884ec2ecbeba953a38
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2020-05-08 17:38:18 +0200

    Abilitato archivio

--- plugin/bacheca/cpt-bacheca.php
+++ plugin/bacheca/cpt-bacheca.php
@@ -49,7 +49,7 @@
 		"show_in_rest" => true,
 		"rest_base" => "cerco-offro",
 		"rest_controller_class" => "WP_REST_Posts_Controller",
-		"has_archive" => false,
+		"has_archive" => true,
 		"show_in_menu" => true,
 		"show_in_nav_menus" => true,
 		"delete_with_user" => false,
