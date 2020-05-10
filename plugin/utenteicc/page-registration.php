c03130dca7fba6ee5e3eaf73642bae4ec02e8ebd
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2020-05-10 23:52:53 +0200

    Separato numero telefono e aggiunto form per iscrizione NL

--- plugin/utenteicc/page-registration.php
+++ plugin/utenteicc/page-registration.php
@@ -19,6 +19,17 @@ if ($user_ID)
 
     if( $_SERVER['REQUEST_METHOD'] == 'POST' )
       {
+        // Check firstname is present
+        if(empty($_POST['firstname']))
+        {
+            $errors['firstname'] = "Il nome non può essere vuoto";
+        }
+
+        // Check lastname is present
+        if(empty($_POST['lastname']))
+        {
+            $errors['lastname'] = "Il cognome non può essere vuoto";
+        }
 
         // Check username is present and not already in use
         $username = $wpdb->escape($_REQUEST['username']);
@@ -82,7 +93,7 @@ if ($user_ID)
             echo " - email: " . $_POST['email'];
             echo " - cap: " . $_POST['cap'];
             echo " - anno_nascita: " . $_POST['anno_nascita'];
-            echo " - telefono: " . $_POST['telefono'];
+            echo " - telefono: " . $_POST['preftelefono'] . " " .$_POST['telefono'];
             echo " - sesso: " . $_POST['sesso'];
             echo " - password: " . $_POST['password'];
             echo " - password_confirmation: " . $_POST['password_confirmation'];
@@ -98,8 +109,47 @@ if ($user_ID)
             wp_mail( $to, $subject, $body, $headers );
             wp_mail( 'ptiozzo@me.com', '[ICC] nuovo utente registrato', $body, $headers );
             ?>
+
+
+            <form id="ml_signup_form" class="mb-2" action="https://b3x1d.emailsp.com/frontend/subscribe.aspx" method="post">
+              <input name="email" type="hidden" value="<?php echo $_POST['email'];?>">
+              <input name="prefix" type="hidden" value="<?php echo $_POST['preftelefono'];?>" />
+              <input name="number" type="hidden" value="<?php echo $_POST['telefono'];?>" />
+              <input name="campo1" type="hidden" value="<?php echo $_POST['firstname'];?>" />
+              <input name="campo2" type="hidden" value="<?php echo $_POST['lastname'];?>" />
+              <input name="campo6" type="hidden" value="<?php echo $_POST['cap'];?>" />
+              <input name="campo28" type="hidden" value="<?php echo $_POST['sesso'];?>" />
+              <input name="campo29" type="hidden" value="<?php echo $_POST['anno_nascita'];?>" />
+
+              <div class="form-check">
+                <input name="list" type="checkbox" value="3">
+                <label class="form-check-label" for="gridCheck1">
+                  La newsletter settimanale di ItaliaCheCambia
+                </label>
+              </div>
+              <div class="form-check">
+                <input name="list" type="checkbox" value="13">
+                <label class="form-check-label" for="gridCheck1">
+                  La newsletter mensile di PiemonteCheCambia
+                </label>
+              </div>
+              <div class="form-check">
+                <input name="list" type="checkbox" value="16">
+                <label class="form-check-label" for="gridCheck1">
+                  La newsletter mensile di CasentinoCheCambia
+                </label>
+              </div>
+              <div class="form-check">
+                <input name="list" type="checkbox" value="15">
+                <label class="form-check-label" for="gridCheck1">
+                  La newsletter giornaliera di #IoNonMiRassegno
+                </label>
+              </div>
+              <input type="submit" class="btn btn-primary ml-3 mt-2 " id="submitbtn" name="submit" value="Iscriviti" />
+            </form>
+
             <div class="alert alert-success" role="alert">
-              Registrazione avvenuta con successo
+              Registrazione avvenuta con successo. Effettua il <a href="/wp-login.php?redirect_to=/">login</a> o torna alla <a href="/">home page</a>
             </div>
             <?php
         }
@@ -116,54 +166,57 @@ if ($user_ID)
               ?>
             </ul>
           </div>
+
           <?php
         }
 
     }
 }
-
+if($success != 1){
 ?>
 
-
-
-<form id="wp_signup_form" class="mb-2" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+ <form id="wp_signup_form" class="mb-2" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
   <div class="row m-0">
         <div class="form-group col-12 col-md-6">
           <label for="firstname">Nome</label>
-          <input type="text" name="firstname" class="form-control" id="firstname" aria-describedby="firstname">
+          <input type="text" name="firstname" class="form-control" id="firstname" aria-describedby="firstname" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["firstname"].'"';} ?>>
         </div>
         <div class="form-group col-12 col-md-6">
           <label for="lastname">Cognome</label>
-          <input type="text" name="lastname" class="form-control" id="lastname" aria-describedby="lastname">
+          <input type="text" name="lastname" class="form-control" id="lastname" aria-describedby="lastname" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["lastname"].'"';} ?>>
         </div>
         <div class="form-group col-12 col-md-6">
           <label for="username">Scegli il tuo nome utente</label>
-          <input type="text" name="username" class="form-control" id="username" aria-describedby="username">
+          <input type="text" name="username" class="form-control" id="username" aria-describedby="username" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["username"].'"';} ?>>
           <small id="email" class="form-text text-muted">Lo username ti servirà per accedere alla piattaforma.</small>
         </div>
         <div class="form-group col-12 col-md-6">
           <label for="Email1">Indirizzo mail</label>
-          <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+          <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["email"].'"';} ?>>
           <small id="email" class="form-text text-muted">Non condivideremo con nessuno questo indirizzo mail.</small>
         </div>
         <div class="form-group col-12 col-md-6">
           <label for="cap">CAP</label>
-          <input type="text" name="cap" class="form-control" id="cap" aria-describedby="cap">
+          <input type="text" name="cap" class="form-control" id="cap" aria-describedby="cap" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["cap"].'"';} ?>>
         </div>
         <div class="form-group col-12 col-md-6">
           <label for="anno_nascita">Anno di nascita</label>
-          <input type="text" name="anno_nascita" class="form-control" id="anno_nascita" aria-describedby="anno_nascita">
+          <input type="text" name="anno_nascita" class="form-control" id="anno_nascita" aria-describedby="anno_nascita" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["anno_nascita"].'"';} ?>>
         </div>
-        <div class="form-group col-12 col-md-6">
+        <div class="form-group col-4 col-md-2">
+          <label for="preftelefono">Prefisso</label>
+          <input type="text" name="preftelefono" class="form-control" id="telefono" aria-describedby="telefono" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["preftelefono"].'"';}else{echo 'value=+39';} ?>>
+        </div>
+        <div class="form-group col-8 col-md-4">
           <label for="telefono">Numero di telefono</label>
-          <input type="text" name="telefono" class="form-control" id="telefono" aria-describedby="telefono" value="+39">
+          <input type="text" name="telefono" class="form-control" id="telefono" aria-describedby="telefono" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' ){echo 'value="'.$_POST["telefono"].'"';} ?>>
         </div>
         <div class="form-group col-12 col-md-6">
           <label for="sesso">Sesso</label>
           <select name="sesso" class="form-control" id="sesso">
-            <option value="-">-</option>
-            <option value="M">M</option>
-            <option value="F">F</option>
+            <option value="-" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['sesso'] == "-" ){echo "selected";} ?>>-</option>
+            <option value="M" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['sesso'] == "M" ){echo "selected";} ?>>M</option>
+            <option value="F" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['sesso'] == "F" ){echo "selected";} ?>>F</option>
           </select>
         </div>
         <div class="form-group col-12 col-md-6">
@@ -177,7 +230,7 @@ if ($user_ID)
           <small id="email" class="form-text text-muted">Ripeti la tua password.</small>
         </div>
         <div class="form-check col-12 ml-3">
-          <input class="form-check-input" name="terms" type="checkbox" value="Yes" id="terms">
+          <input class="form-check-input" name="terms" type="checkbox" value="Yes" id="terms" <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['terms'] == "Yes" ){echo "checked";} ?>>
           <label class="form-check-label" for="terms">
             Accetto i termini e le condizioni
           </label>
@@ -186,5 +239,6 @@ if ($user_ID)
         <input type="submit" class="btn btn-primary ml-3 mt-2 " id="submitbtn" name="submit" value="Registrati" />
   </div>
 </form>
+<?php } ?>
 </div>
 <?php get_footer(); ?>
