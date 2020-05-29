46e78297dcd42eae7d585ceb217cc108eef1537c
Paolino <paolino@MacBook-Pro-Paolino--Fra.local>
2020-05-30 00:59:55 +0200

    Gestione dell'editing

--- plugin/bacheca/new-cercooffro.php
+++ plugin/bacheca/new-cercooffro.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<div class="container mx-0">
+<div class="container">
 
 
 <h1><?php the_title(); ?></h1>
@@ -40,66 +40,69 @@ $errors = array();
     $errors['content'] = "Devi inserire un contenuto di almeno 6 parole";
   }
 
-  if(strpos($_FILES['image']["type"],'image') === false){
-    $errors['image'] = "Il file caricato non è un'immagine";
+  if($_FILES['image']['size'] != 0){
+    if(strpos($_FILES['image']["type"],'image') === false){
+      $errors['image'] = "Il file caricato non è un'immagine";
+    }
   }
 
 
 
   if(0 === count($errors)){
 
-    global $current_user;
-		get_currentuserinfo();
-
-		$user_login = $current_user->user_login;
-		$user_email = $current_user->user_email;
-		$user_firstname = $current_user->user_firstname;
-		$user_lastname = $current_user->user_lastname;
-		$user_id = $current_user->ID;
-
-    $post_title = $_POST['tipologia']."...".$_POST['titolo'];
-
-    $new_post = array(
-			'post_title' => ucfirst($post_title),
-			'post_content' => $_POST['content'],
-			'post_status' => 'publish',
-			'post_name' => $_POST['titolo'],
-			'post_type' => 'cerco-offro',
-      'comment_status' => 'open'
-		);
-
-    $post_id = wp_insert_post($new_post);
-
-    wp_set_object_terms($post_id,$_POST['tematica'],'tematica');
-    wp_set_object_terms($post_id,$_POST['regione'],'regione');
-    wp_set_object_terms($post_id,$_POST['tipologia'],'cercooffro');
-
-
-    if ( isset($_FILES['image']) ) {
-        $upload = wp_upload_bits($_FILES["image"]["name"], null, file_get_contents($_FILES["image"]["tmp_name"]));
-
-        if ( ! $upload_file['error'] ) {
-
-            $filename = $upload['file'];
-            $wp_filetype = wp_check_filetype($filename, null);
-            $attachment = array(
-                'post_mime_type' => $wp_filetype['type'],
-                'post_title' => sanitize_file_name($filename),
-                'post_content' => '',
-                'post_status' => 'inherit'
-            );
-
-            $attachment_id = wp_insert_attachment( $attachment, $filename, $post_id );
-
-            if ( ! is_wp_error( $attachment_id ) ) {
-                require_once(ABSPATH . 'wp-admin/includes/image.php');
-
-                $attachment_data = wp_generate_attachment_metadata( $attachment_id, $filename );
-                wp_update_attachment_metadata( $attachment_id, $attachment_data );
-                set_post_thumbnail( $post_id, $attachment_id );
-            }
-        }
+    if($_GET['action'] == 'edit' && isset($_GET['postID'])){
+      $new_post = array(
+        'ID' => $_GET['postID'],
+  			'post_title' => $_POST['titolo'],
+  			'post_content' => $_POST['content'],
+  			'post_status' => 'publish',
+  			'post_name' => $_POST['titolo'],
+  			'post_type' => 'cerco-offro',
+        'comment_status' => 'open'
+  		);
+      $post_id = wp_update_post($new_post);
+    } else {
+      $post_title = $_POST['tipologia']."...".$_POST['titolo'];
+      $new_post = array(
+  			'post_title' => ucfirst($post_title),
+  			'post_content' => $_POST['content'],
+  			'post_status' => 'publish',
+  			'post_name' => $_POST['titolo'],
+  			'post_type' => 'cerco-offro',
+        'comment_status' => 'open'
+  		);
+      $post_id = wp_insert_post($new_post);
     }
+      wp_set_object_terms($post_id,$_POST['tematica'],'tematica');
+      wp_set_object_terms($post_id,$_POST['regione'],'regione');
+      wp_set_object_terms($post_id,$_POST['tipologia'],'cercooffro');
+
+      if ( isset($_FILES['image']) && $_FILES['image']['size'] != 0 ) {
+          $upload = wp_upload_bits($_FILES["image"]["name"], null, file_get_contents($_FILES["image"]["tmp_name"]));
+
+          if ( ! $upload_file['error'] ) {
+
+              $filename = $upload['file'];
+              $wp_filetype = wp_check_filetype($filename, null);
+              $attachment = array(
+                  'post_mime_type' => $wp_filetype['type'],
+                  'post_title' => sanitize_file_name($filename),
+                  'post_content' => '',
+                  'post_status' => 'inherit'
+              );
+
+              $attachment_id = wp_insert_attachment( $attachment, $filename, $post_id );
+
+              if ( ! is_wp_error( $attachment_id ) ) {
+                  require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+                  $attachment_data = wp_generate_attachment_metadata( $attachment_id, $filename );
+                  wp_update_attachment_metadata( $attachment_id, $attachment_data );
+                  set_post_thumbnail( $post_id, $attachment_id );
+              }
+          }
+      }
+
 
     $url = get_post_permalink($post_id);
 
@@ -138,16 +141,49 @@ $errors = array();
 
 if($success != 1 && is_user_logged_in() ){
 
+  if($_GET['action'] == 'edit' && isset($_GET['postID'])){
+    $form_invia = "Aggiorna cerco/offro";
+    $args = array(
+      'p' => $_GET['postID'],
+      'post_type' => 'cerco-offro',
+    );
+    $query = new WP_Query( $args );
+    //var_dump($query);
+    if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) {
+          $query->the_post();
+          $form_titolo = get_the_title();
+          $form_regioni = get_the_terms( get_the_ID() , 'regione' )[0]->slug;
+          $form_tipologia = get_the_terms( get_the_ID() , 'cercooffro' )[0]->slug;
+          $form_tematica = get_the_terms( get_the_ID() , 'tematica' )[0]->slug;
+          $form_content = get_the_content();
+
+      }
+    }
+    wp_reset_postdata();
+  } else {
+    $form_tipologia = $_POST['tipologia'];
+    $form_regioni = $_POST['regioini'];
+    $form_tematica = $_POST['tematica'];
+    $form_titolo = $_POST['titolo'];
+    $form_content = $_POST['content'];
+    $form_invia = "Aggiungi cerco/offro";
+  }
+
+
+
+
+
   ?>
   <form class="mt-3 mb-2 form-inline" method="post" action="<?php echo get_pagenum_link(); ?>" enctype="multipart/form-data">
     <select name="tipologia" class="custom-select" >
-      <option value="cercooffro" <?php if ($_POST['tipologia'] == "cercooffro") {echo 'selected';} ?>>Cerco o Offro?</option>
+      <option value="cercooffro" <?php if ($form_tipologia == "cercooffro") {echo 'selected';} ?>>Cerco o Offro?</option>
       <?php
         $categories = get_terms( array('taxonomy' => 'cercooffro','hide_empty' => false,'orderby'=> 'slug','order' => 'ASC'));
         foreach ($categories as $category) {
           if($category->slug != 'risolto'){
             $option = '<option value="'.$category->slug.'" ';
-            if ($_POST['tipologia'] == $category->slug) {$option .= 'selected ';};
+            if ($form_tipologia == $category->slug) {$option .= 'selected ';};
             $option .= '>'.$category->name;
             $option .= '</option>';
             echo $option;
@@ -160,12 +196,12 @@ if($success != 1 && is_user_logged_in() ){
         <?php
           $categories = get_terms( array('taxonomy' => 'regione','hide_empty' => false,'orderby'=> 'slug','order' => 'ASC'));
           ?>
-          <option value="_tutteleregioni" <?php if ($_POST['tematica'] == "_tutteleregioni") {echo 'selected';} ?>>Seleziona la regione</option>
+          <option value="_tutteleregioni" <?php if ($form_regioni == "_tutteleregioni") {echo 'selected';} ?>>Seleziona la regione</option>
           <?php
           foreach ($categories as $category) {
             if($category->slug != "_tutteleregioni"){
               $option = '<option value="'.$category->slug.'" ';
-              if ($_POST['regione'] == $category->slug) {$option .= 'selected ';};
+              if ($form_regioni == $category->slug) {$option .= 'selected ';};
               $option .= '>'.$category->name;
               $option .= '</option>';
               echo $option;
@@ -174,12 +210,12 @@ if($success != 1 && is_user_logged_in() ){
         ?>
       </select>
     <select name="tematica" class="custom-select mx-2">
-      <option value="tutteletematiche" <?php if ($_POST['tematica'] == "tutteletematiche") {echo 'selected';} ?>>Seleziona la tematica</option>
+      <option value="tutteletematiche" <?php if ($form_tematica == "tutteletematiche") {echo 'selected';} ?>>Seleziona la tematica</option>
       <?php
         $categories = get_terms( array('taxonomy' => 'tematica','hide_empty' => false,'orderby'=> 'slug','order' => 'ASC'));
         foreach ($categories as $category) {
           $option = '<option value="'.$category->slug.'" ';
-          if ($_POST['tematica'] == $category->slug) {$option .= 'selected ';};
+          if ($form_tematica == $category->slug) {$option .= 'selected ';};
           $option .= '>'.$category->name;
           $option .= '</option>';
           echo $option;
@@ -187,11 +223,11 @@ if($success != 1 && is_user_logged_in() ){
       ?>
     </select>
     <div class="form-group my-2 col-12 d-block px-0">
-      <input id="titolo" class="form-control w-75" type="text" name="titolo" placeholder="Inserisci il titolo del tuo cerco/offro" <?php echo 'value="'.$_POST['titolo'] .'"';?>>
+      <input id="titolo" class="form-control w-75" type="text" name="titolo" placeholder="Inserisci il titolo del tuo cerco/offro" <?php echo 'value="'.$form_titolo .'"';?>>
       <small id="titoloHelp" class="form-text text-muted">Non iniziare con cerco o offro, verranno aggiunti automaticamente.</small>
     </div>
     <div class="form-group my-2 col-12 d-block px-0">
-      <textarea id="content" class="form-control w-75" type="text" name="content" placeholder="Inserisci il tuo annuncio qui" rows="5"><?php echo $_POST['content'];?></textarea>
+      <textarea id="content" class="form-control w-75" type="text" name="content" placeholder="Inserisci il tuo annuncio qui" rows="5"><?php echo $form_content;?></textarea>
       <small id="contentHelp" class="form-text text-muted">Questo sarà il testo del tuo annuncio.</small>
     </div>
     <div class="form-group my-2 col-12">
@@ -199,7 +235,7 @@ if($success != 1 && is_user_logged_in() ){
       <input type="file" name="image" class="form-control-file" id="image">
     </div>
 
-    <input name="submit_button" type="Submit" value="Aggiunti cerco/offro" class="btn btn-secondary">
+    <input name="submit_button" type="Submit" value="<?php echo $form_invia;?>" class="btn btn-secondary">
   </form>
 
 <?php } ?>
