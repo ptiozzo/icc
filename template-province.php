bd4641190b3feedf426cd9042fb53c148f8e88c7
Paolo <paolo@EDP4YOUs-MacBook-Air.local>
2020-07-01 14:43:28 +0200

    Aggiornate etichette

--- template-province.php
+++ template-province.php
@@ -67,19 +67,7 @@ Template Name: Template province
                   <span class="ml-4"><?php the_time('j M Y') ?></span>
                   <span>
                     <?php
-                      if (in_category('documentari')) {
-                        echo 'I documentari';
-                      } elseif (in_category('io-faccio-cosi')) {
-                        echo 'Io faccio così';
-                      }elseif (in_category('meme')) {
-                        echo 'I meme';
-                      }elseif (in_category('rubriche')) {
-                        echo 'Le rubriche';
-                      }elseif (in_category('salute-che-cambia')) {
-                        echo 'Salute';
-                      }elseif (in_category('articoli')) {
-                        echo 'Gli Articoli';
-                      }
+                      get_template_part('inc/post','etichetta');
                     ?>
                   </span>
                 </div>
