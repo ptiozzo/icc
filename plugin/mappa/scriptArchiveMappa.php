23368f2d9c92b567d40be060f1744bd82b3bfe4e
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2020-08-02 00:05:53 +0200

    Cambiato tipo di cartina

--- plugin/mappa/scriptArchiveMappa.php
+++ plugin/mappa/scriptArchiveMappa.php
@@ -119,7 +119,7 @@ if( !$loopMappaArchivio->have_posts()){
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: 'mapbox/outdoors-v11',
+      id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoiaWNjLW1hcHBhIiwiYSI6ImNrYmpzNWZkcTByeXAzMXBqaGRzM2dmaWoifQ.TYuCegt1hW_2z5qyjDBZkg'
