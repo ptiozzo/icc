d3833a75717ad6340556dcd8c252f89657c30281
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2020-08-05 21:24:21 +0200

    RiFix import

--- plugin/mappa/import-mappa.php
+++ plugin/mappa/import-mappa.php
@@ -1,6 +1,6 @@
 <?php get_header();
 
-$daImportare = 3000;
+$daImportare = 100;
 
 if ( ! function_exists( 'post_exists' ) ) {
     require_once( ABSPATH . 'wp-admin/includes/post.php' );
@@ -22,11 +22,29 @@ if (($handle = fopen($myFile, "r")) !== FALSE) {
       echo "<tr style='border: 1px solid grey; min-width: 10px;'>";
       $num = count($data);
       echo "<td style='border: 1px solid grey; min-width: 10px;'>".$row . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[0] . "</td>\n";
+      /*echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[1] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[2] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[3] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[4] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[5] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[6] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[7] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[8] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[9] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[10] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[11] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[12] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[13] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[14] . "</td>\n";
+      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[15] . "</td>\n";
+    */  echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[16] . "</td>\n";
+
       //stato da splittare
       $post_id = post_exists( ucfirst($data[0]),'','','mappa');
       $var = explode(",",$data[16]);
-      $varNum = count($var)-1;
-      if ($varNum >= 1){
+      $varNum = count($var);
+      if ($varNum >= 1 && $post_id != 0){
         for ($i=0; $i < $varNum; $i++) {
           $var[$i] = trim($var[$i]);
           if($i == 0){
@@ -39,12 +57,14 @@ if (($handle = fopen($myFile, "r")) !== FALSE) {
       echo "<td style='border: 1px solid grey; min-width: 10px;'>".$post_id."</td>";
 
 
-      echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[0] . "</td>\n";//nome realtà
+      //echo "<td style='border: 1px solid grey; min-width: 10px;'>".$data[0] . "</td>\n";//nome realtà
 
       //da splittare Stato
       $var = explode(",",$data[16]);
-      $varNum = count($var)-1;
+      $varNum = count($var);
+
       echo "<td style='border: 1px solid grey; min-width: 10px;'><ul>";
+      echo $varNum." ";
       for ($i=0; $i < $varNum; $i++) {
           echo "<li>".$var[$i]."</li>";
       }
