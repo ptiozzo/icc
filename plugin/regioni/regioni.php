61ec4f93ff15c326d0e6fcfa81307097f080a398
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2020-09-30 21:39:50 +0200

    Gestioone regioni attive

---
+++ plugin/regioni/regioni.php
@@ -0,0 +1,30 @@
+<?php
+
+add_action( 'admin_menu', 'icc_menu_regioni_admin' );
+function icc_menu_regioni_admin()
+{
+  add_submenu_page(
+    'icc-theme',
+    'ICC Regioni',
+    'Regioni',
+    'edit_posts',
+    'icc-regioni-setting',
+    'icc_menu_admin_regioni_isctruction'
+  );
+}
+
+function icc_menu_admin_regioni_isctruction()
+{
+  require 'admin-regioni.php';
+}
+
+function icc_is_region_active($regione){
+  $regioni = get_option('icc_regioni_attive');
+  if(in_array(strtolower($regione),$regioni)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+?>
