02ec282052e44c04adcc6e594cbfdae3b634105b
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2021-01-18 21:13:51 +0100

    Aggiunto template

--- inc/rewrite.php
+++ inc/rewrite.php
@@ -27,6 +27,7 @@ function myplugin_rewrite_rule() {
 	add_rewrite_rule( '^piemonte/calendario/?', 'index.php?page_id=57957','top' );
 	add_rewrite_rule( '^piemonte/contattaci/?', 'index.php?page_id=41204','top' );
 	add_rewrite_rule( '^piemonte/mappa/?', 'index.php?page_id=57961','top' );
+	add_rewrite_rule( '^piemonte/visione-2040-cuneo/?', 'index.php?page_id=72762','top' );
 	add_rewrite_rule( '^piemonte/pianetafuturo/?', 'index.php?page_id=64470','top' );
 	add_rewrite_rule( '^piemonte/storie/page/?([0-9]{1,})/?', 'index.php?page_id=41191&paged=$matches[1]','top' );
 	add_rewrite_rule( '^piemonte/storie/?', 'index.php?page_id=41191','top' );
---
+++ piemonte/visione2040cuneo.php
@@ -0,0 +1,348 @@
+<?php
+/*
+Template Name: Visione 2040 cuneo
+*/
+?>
+
+<?php get_header(); ?>
+
+<main class="visione">
+	<section class="head">
+		<h1>Visione 2040 Cuneo</h1>
+	</section>
+
+	<section class="content">
+		<div class='text'>
+			Nel 2015, 100 ambasciatori e ambasciatrici provenienti da associazioni, imprese, mondo accademico, hanno lavorato per tre mesi attorno a diciassette tavoli. Ne sono usciti altrettanti documenti tematici che presentano la fotografia attuale del nostro Paese, una visione comune di come potrà essere l’Italia nel 2040 e delle proposte per arrivarci, sia a livello nazionale che a livello individuale. Inoltre una serie di azioni concrete che ciascuno di noi può fare fin da oggi per contribuire a questo grande processo di cambiamento. Qua sotto trovate i 17 documenti, frutto di questo enorme lavoro! Inoltre nel marzo 2018 è uscito il libro “E ora si cambia”, che raccoglie i documenti riassunti ed aggiornati, a cura di Andrea Degl’Innocenti e Daniel Tarozzi. Nota: spesso all’interno dei documenti, per facilitarne la lettura, abbiamo usato sostantivi al maschile, che non si intendono però vincolati a tale genere.
+		</div>
+
+		<div class="list">
+			<ul>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/home.svg' title='' alt=''>
+							</figure>
+							<span>Abitare</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Negli appartamenti vivevamo appartati, oggi invece viviamo più assieme, condividendo spazi e abitudini. In molti si sono spostati dalle metropoli verso le campagne e i piccoli centri. Abbiamo case passive o a basso consumo energetico e si è praticamente smesso di costruirne di nuove, recuperando gli immobili abbandonati e riqualificando quelli esistenti.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-abitare/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/water.svg' title='' alt=''>
+							</figure>
+							<span>Agricoltura<br>e Acqua</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Abbiamo accesso sicuro all’acqua, a un cibo sano, sufficiente e nutriente, prodotto localmente. Sovranità alimentare! Ecco il principio che guida la produzione e il consumo di cibo nel 2040. Sono in via di estinzione le monocolture, sostituite dalla biodiversità dei territori, coltivati con metodi alternativi e genuini.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-agricoltura-e-acqua/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/enviroment.svg' title='' alt=''>
+							</figure>
+							<span>Ambiente</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									È in atto un profondo mutamento culturale: stiamo uscendo dalla cultura antropocentrica per ritrovare il nostro posto all’interno degli ecosistemi e non più al di sopra di essi. L’Italia è un paese a consumo di suolo zero e una rete di aree protette tutela il Mediterraneo dallo sfruttamento dovuto a pesca, trivellazioni, ecc.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-ambiente/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/recycle.svg' title='' alt=''>
+							</figure>
+							<span>Cicli Riproduttivi<br>e Rifiuti</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									I rifiuti non esistono più, e con essi sono spariti gli inceneritori e le discariche: oggi gli scarti di una determinata produzione diventano materia prima per un’altra produzione. L’evoluzione delle stampanti 3d e dei Fablab ha abbattutto la produzione di risorse inutili e l’obsolescenza pianificata è stata spazzata via, sostituita da prodotti duraturi e facilmente riparabili.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-cicli-produttivi-e-rifiuti/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/weather.svg' title='' alt=''>
+							</figure>
+							<span>Clima</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Negli anni passati abbiamo abbattuto le emissioni da parte dell’industria, delle abitazioni private, dei mezzi di trasporto e preso misure di contenimento come la riforestazione e la prevenzione del dissesto idrogeologico. Le conseguenze dei cambiamenti climatici sono ormai evidenti, ma sembra probabile che l’innalzamento delle temperature resterà entro i 2°C.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-clima/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/disability.svg' title='' alt=''>
+							</figure>
+							<span>Disabilità</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Le barriere culturali e fisiche che impedivano una completa integrazione delle persone disabili non esistono più. La disabilità non è più vissuta come un problema o un elemento discriminante, ma come una caratteristica distintiva del’individuo.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-disabilita/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/economy.svg' title='' alt=''>
+							</figure>
+							<span>Economia</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Un’economia circolare, centrata sul benessere dell’individuo, della comunità, degli ecosistemi e su una più equa ripartizione della ricchezza. Dalla crisi del capitalismo globale e finanziarizzato è nato un nuovo modello che, su piccola scala, affianca agli strumenti economici classici monete locali e complementari, economia del dono e della condivisione.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-economia/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/education.svg' title='' alt=''>
+							</figure>
+							<span>Educazione</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Quanto si può imparare dalla Natura? La scuola del 2040 è basata sulla semplicità, sul rapporto con la Natura e su un’alimentazione sana. Esiste ancora una solida struttura di teorie pedagogiche, ma si presenta in forma esperienziale, prediligendo l’apprendimento ludico, basato su progetti e attività concrete.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-educazione	/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/energy.svg' title='' alt=''>
+							</figure>
+							<span>Energia</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Le energie fossili sono praticamente scomparse e oggi l’Italia si alimenta con il 100% di rinnovabili prodotte nel nostro paese. Abbiamo raggiunto la sovranità energetica grazie a una politica basata su tre parole chiave: Rinnovabili, Risparmio ed Efficienza energetica.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-energia/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/business.svg' title='' alt=''>
+							</figure>
+							<span>Imprenditoria</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Fare impresa nel 2040 significa lavorare in sinergia con i territori e le comunità, nel rispetto dell’ambiente e della biodiversià. Oggi le imprese sono focalizzate sull’utilizzo ottimale delle risorse ambientali, sociali e tecnologiche. L’ottica della collaborazione si è affermata e materiali, informazioni e uomini sono connessi in rete e condivisi tra aziende differenti.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-imprenditoria/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/information.svg' title='' alt=''>
+							</figure>
+							<span>Informazione<br> e Comunicazione</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Il ruolo degli editori è stato fortemente ridimensionato, i monopoli aboliti, mentre i giornalisti, uniti in consorzi o liberi professionisti, sono diventati i veri protagonisti di un’informazione liberata dal controllo di lobby e poteri. Il ruolo del cittadino attivo è determinante e l’educazione ai media è insegnata nelle scuole primarie e secondarie.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-informazione-e-comunicazione/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/work.svg' title='' alt=''>
+							</figure>
+							<span>Lavoro</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Lavoriamo di meno e facciamo lavori più soddisfacenti. In molti autoproducono parte di ciò che gli serve per vivere e sono aumentate le economie informali e di comunità. Non esiste più il lavoro fine a se stesso, inutile o tantomeno dannoso: oggi il lavoro si basa su utilità sociale, relazioni, realizzazione personale, dignità ed etica.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-lavoro/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/legality.svg' title='' alt=''>
+							</figure>
+							<span>Legalità</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Le associazioni accedono con facilità ai beni confiscati alle mafie e sono state superate le criticità legate alle lungaggini burocratiche e alle ipoteche bancarie. I cittadini e le imprese riconoscono allo Stato l’impegno nella creazione di strumenti efficaci per contrastare corruzione, falso in bilancio, autoriciclaggio. E’ in atto una rivoluzione culturale collettiva contro le mafie.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-legalita/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/mobility.svg' title='' alt=''>
+							</figure>
+							<span>Mobilità</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									La mobilità in Italia è stata ripensata ed oggi è basata sulla sostenibilità economica e ambientale e sul riequilibrio fra Nord e Sud del Paese, fra metropoli e città minori, fra viaggiatori ricchi viaggiatori poveri, gomma e rotaia, mobilità motorizzata e dolce. Molto ridotto il traffico aereo, si è puntato con successo su una rete ferroviaria pubblica capillare ed efficiente.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-mobilita/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/health.svg' title='' alt=''>
+							</figure>
+							<span>Salute</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									La Salute è riconosciuta come il naturale percorso di conoscenza e consapevolezza di ciascuno di noi in relazione alla propria comunità e all’ambiente in cui viviamo. Abbiamo attivato centri di cultura e supporto alla Salute locali e partecipati dove possiamo informarci, praticare attività e incontrare i professionisti scelti dalla stessa comunità.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-salute/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/gender.svg' title='' alt=''>
+							</figure>
+							<span>Tematiche di Genere</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Nell’Italia del 2040 le persone sono in grado di guardarsi l’un l’altra a prescindere da pregiudizi e discriminazione. Oggi possiamo promuovere reali azioni di pari opportunità, uscire da ruoli stereotipati e da un mentalità che vede nell’altr* una minaccia qualora esca dagli schemi.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-tematiche-di-genere/" class="stretched-link"></a>
+
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="box">
+						<div class="box-head">
+							<figure>
+								<img src='<?php echo get_template_directory_uri();?>/assets/img/icons/travel.svg' title='' alt=''>
+							</figure>
+							<span>Viaggiare</span>
+						</div>
+						<div class="box-body">
+							<article>
+								<p>
+									Dire turismo è dire viaggio, conoscenza, responsabilità, cultura. Un turismo per tutti, responsabile, etico, sostenibile fatto da viaggiatori consapevoli e proposto da una rete di operatori coesi nei valori, nell’immagine e operativamente sinergici.
+								</p>
+							</article>
+							<a href="<?php echo home_url(); ?>/visione-2040-viaggiare/" class="stretched-link"></a>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</section>
+</main>
+
+<?php get_footer(); ?>
