99b08d6f5268b99711696e3d29f04668c7353427
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2021-03-10 09:10:16 +0100

    Update single.php

--- single.php
+++ single.php
@@ -180,12 +180,12 @@
 		 			<?php
 		 			if( !empty (get_post_meta( get_the_ID(), 'YouTubeLink',true))){
 		 				?>
-					 	<!-- Thumbnail o video youtube -->
+					 	<!-- Thumbnail o video youtube
 		 				<div class="single__thumbnail">
 		 					<figure class="embed-responsive embed-responsive-16by9">
 		 						<iframe width="800" height="480" src="https://www.youtube.com/embed/<?php echo linkifyYouTubeURLs(get_post_meta( get_the_ID(), 'YouTubeLink',true));?>?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
-		 					</figure>
-		 				</div>
+		 					</figure> 
+		 				</div> -->
 		 				<?php
 		 			}
 		 			?>
