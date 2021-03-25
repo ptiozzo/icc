3f75673d742fe74679ff7a8a92348f802e3ec945
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2021-03-25 22:28:45 +0100

    Fix opening menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,21 @@
 
   (function(){
 
-    $(".menu-item-has-children").click(function(e){
-        $(this).toggleClass("show")
-    })
+    $(".menu-item-has-children").on('click', function (e){
+      e.stopPropagation();
+      $(this).toggleClass('show');
+    });
+
 
   })();
+
+  var specifiedElement = document.getElementById('navbarHome');
+    document.addEventListener('click', function(event) {
+        var isClickInside = specifiedElement.contains(event.target);
+        if (!isClickInside) {
+          $(".menu-item-has-children").removeClass('show')
+        }
+    });
 
   $(".mappa_reset").click(function() {
     document.cookie="PHPSESSID='';path='/'";
