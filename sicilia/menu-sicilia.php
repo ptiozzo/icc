2af1e0807b15df67d7ca993241a30c05b0e7c8b8
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2021-05-07 21:09:16 +0200

    Fix titolo modal

--- sicilia/menu-sicilia.php
+++ sicilia/menu-sicilia.php
@@ -129,7 +129,7 @@ if($loopSiciliaDonazione->have_posts()):
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="SiciliaDonazione">Donazione</h5>
+        <h5 class="modal-title" id="SiciliaDonazione">Contribuisci</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
