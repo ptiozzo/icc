fa5ee24f9000bcfeea38885c871e91a665c0a456
ptiozzo <33660629+ptiozzo@users.noreply.github.com>
2021-05-08 22:28:06 +0200

    Gestione rimozione widget

--- plugin/tag-che-cambia/admin-tag.php
+++ plugin/tag-che-cambia/admin-tag.php
@@ -1,10 +1,25 @@
 <h1>Gestione TAG</h1>
 
-<?php $TagAttivi = get_option("icc_tagCheCambia_attivi") ? get_option("icc_tagCheCambia_attivi") : array(); ?>
-
 <?php
-  /* ---- Aggiungo nuova voce ai TAG ---- */
+/* ---- Rimuovi nuova voce ai TAG ---- */
+if ($_POST['icc_tagCheCambia_attivi_Rimuovi']){
+  $TagAttivi = get_option("icc_tagCheCambia_attivi") ? get_option("icc_tagCheCambia_attivi") : array();
+
+  foreach ($TagAttivi as $tag){
+    if($_POST['icc_tagCheCambia_attivi_Rimuovi'] != "Rimuovi ".$tag['tagName']){
+      $TagAttiviNew[] = array('tagName' => $tag['tagName']);
+    }
+  }
+
+  update_option('icc_tagCheCambia_attivi',$TagAttiviNew,'no');
+}
+?>
+
+<!-- Aggiungo nuova voce ai TAG -->
+<?php $TagAttivi = get_option("icc_tagCheCambia_attivi") ? get_option("icc_tagCheCambia_attivi") : array(); ?>
+<?php
   if($_POST["icc_tagCheCambia_Add"]){
+
     $TagAttivi = get_option("icc_tagCheCambia_attivi") ? get_option("icc_tagCheCambia_attivi") : array();
     $TagAttivi[] = array('tagName' => $_POST['icc_tagCheCambia_attivi']);
 
@@ -13,8 +28,8 @@
 
 ?>
 
+<!-- FORM per aggiunta nuova voce ai TAG -->
 <?php $TagAttivi = get_option("icc_tagCheCambia_attivi") ? get_option("icc_tagCheCambia_attivi") : array();  ?>
-
 <form class="" action="<?php echo get_pagenum_link(); ?>" method="post">
   <select class="" name="icc_tagCheCambia_attivi">
     <?php
@@ -26,10 +41,7 @@
           <option value="<?php echo $tag->slug; ?>"><?php echo $tag->name; ?></option>
           <?php
         }
-
     ?>
-
-
     <?php endforeach; ?>
   </select>
   <input name="icc_tagCheCambia_Add" type="Submit" value="Aggiungi" class="button">
@@ -37,21 +49,17 @@
 
 <br><br>
 
+<!-- Form di riepilogo/rimozione -->
 <form class="" action="<?php echo get_pagenum_link(); ?>" method="post">
 <?php
 
 $TagAttivi = get_option("icc_tagCheCambia_attivi") ? get_option("icc_tagCheCambia_attivi") : array();
-
 foreach ($TagAttivi as $tag) {
-  if($tag['tagName'] == 'default'){
-    echo '<br><br>';
-  }
   echo '<input type="text" name="'.$tag['tagName'].'" value="'.$tag['tagName'].'" readonly>';
-
+  echo '<input type="submit" name="icc_tagCheCambia_attivi_Rimuovi" value="Rimuovi '.$tag['tagName'].'">';
   echo "<br>";
 }
 
  ?>
  <br>
- <input name="icc_tagCheCambia_attivi" type="Submit" value="Salva" class="button">
 </form>
